bf7dff00d75a603621e32394f36c0b0516032ceb
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-03-23 05:02:07 +0000

    v1.5 Initial Import

---
+++ trunk/rapid-framework/plugins/spring_jdbc/src/javacommon/javacommon/base/BaseSpringJdbcDao.java
@@ -0,0 +1,233 @@
+package javacommon.base;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javacommon.xsqlbuilder.XsqlBuilder;
+import javacommon.xsqlbuilder.XsqlBuilder.XsqlFilterResult;
+import javacommon.xsqlbuilder.safesql.DirectReturnSafeSqlProcesser;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.id.IdentifierGenerationException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.support.incrementer.AbstractSequenceMaxValueIncrementer;
+import org.springframework.jdbc.support.incrementer.DB2SequenceMaxValueIncrementer;
+import org.springframework.jdbc.support.incrementer.OracleSequenceMaxValueIncrementer;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.StringUtils;
+
+import cn.org.rapid_framework.jdbc.dialect.Dialect;
+import cn.org.rapid_framework.page.Page;
+import cn.org.rapid_framework.page.PageRequest;
+import cn.org.rapid_framework.page.impl.JdbcScrollPage;
+import cn.org.rapid_framework.util.MapUtils;
+import cn.org.rapid_framework.util.ObjectUtils;
+import cn.org.rapid_framework.util.SqlRemoveUtils;
+/**
+ * Spring的JDBC基类
+ * @author badqiu
+ *
+ */
+public abstract class BaseSpringJdbcDao extends JdbcDaoSupport implements EntityDao{
+
+	protected final Log log = LogFactory.getLog(getClass());
+
+	protected SimpleJdbcTemplate simpleJdbcTemplate;
+	protected NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	public abstract String getIdentifierPropertyName();
+	
+	public abstract Class getEntityClass();
+	
+	private Dialect dialect;
+	
+	public void setDialect(Dialect d) {
+		this.dialect = d;
+	}
+	
+	@Override
+	protected void initTemplateConfig() {
+		super.initTemplateConfig();
+		if(dialect == null) throw new IllegalStateException("'dialect' property must be not null");
+		simpleJdbcTemplate = new SimpleJdbcTemplate(getDataSource());
+		namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+	}
+	
+	public SimpleJdbcTemplate getSimpleJdbcTemplate() {
+		return simpleJdbcTemplate;
+	}
+
+	public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return namedParameterJdbcTemplate;
+	}
+
+	public Serializable convert2CorrectIdType(Object value) throws SQLException, IdentifierGenerationException {
+		return convert2CorrectIdType(value,getIdentifierPropertyType());
+	}
+	
+	public static Serializable convert2CorrectIdType(Object value,Class type) throws SQLException, IdentifierGenerationException {
+		try {
+			return (Serializable)type.getConstructor(String.class).newInstance(value.toString());
+		}catch(Exception e) {
+			throw new IllegalArgumentException(String.format("convert error,value=%s type=%s",value,type.getCanonicalName()),e);
+		}
+	}
+	
+	protected Object getIdentifierPropertyValue(Object entity) {
+		try {
+			return BeanUtils.getProperty(entity, getIdentifierPropertyName());
+		} catch (Exception e) {
+			ReflectionUtils.handleReflectionException(e);
+			return null;
+		}
+	}
+
+	protected Class getIdentifierPropertyType() {
+		try {
+			return PropertyUtils.getPropertyType(getEntityClass().newInstance(), getIdentifierPropertyName());
+		} catch (Exception e) {
+			ReflectionUtils.handleReflectionException(e);
+			return null;
+		}
+	}
+	
+	/**
+	 * 得到全部数据,但执行分页
+	 * @param pageRequest
+	 * @return
+	 */
+	public Page findAll(String tableName,final PageRequest pageRequest) {
+		return pageQuery("select * from "+tableName+" /~ order by {sortingColumn} [sortingDirection] ~/",pageRequest);
+	}
+	
+	public Page pageQuery(String query,PageRequest pageRequest) {
+		return pageQuery(query,"select count(*) "+SqlRemoveUtils.removeSelect(query),pageRequest);
+	}
+	
+	public Page pageQuery(String query,String countQuery,final PageRequest pageRequest) {
+		Map filters = pageRequest.getFilters();
+		
+		if(StringUtils.hasText(pageRequest.getSortingColumn()))
+			MapUtils.putIfNull(filters, "sortingColumn", pageRequest.getSortingColumn());
+		if(StringUtils.hasText(pageRequest.getSortingDirection()))
+			MapUtils.putIfNull(filters, "sortingDirection", pageRequest.getSortingDirection());
+		
+		XsqlBuilder builder = new XsqlBuilder();
+//		XsqlBuilder builder = new XsqlBuilder(SafeSqlProcesserFactory.getMysql());
+		if(builder.getSafeSqlProcesser().getClass() == DirectReturnSafeSqlProcesser.class) {
+			System.err.println(getClass().getSimpleName()+": 你未开启Sql安全过滤,单引号等转义字符在拼接sql时需要转义,不然会导致Sql注入攻击的安全问题，请使用new XsqlBuilder(SafeSqlProcesserFactory.getDataBaseName())开启安全过滤");
+		}
+		
+		XsqlFilterResult queryXsqlResult = builder.generateHql(query,filters);
+		
+		XsqlFilterResult countQueryXsqlResult = builder.generateHql(countQuery,filters);
+		final int totalCount = getSimpleJdbcTemplate().queryForInt(SqlRemoveUtils.removeOrders(countQueryXsqlResult.getXsql()),countQueryXsqlResult.getAcceptedFilters());
+		
+		String sql = queryXsqlResult.getXsql();
+		Map acceptedFilters = queryXsqlResult.getAcceptedFilters();
+		int limit = pageRequest.getPageSize();
+		int pageNumber = pageRequest.getPageNumber();
+		return pageQuery(sql, acceptedFilters, totalCount, limit, pageNumber);
+	}
+
+	private Page pageQuery(String sql, Map paramMap,final int totalCount, int limit, int pageNumber) {
+		if(dialect != null && dialect.supportsLimit()) {
+			if(dialect.supportsLimitOffset()) {
+				Page page = new Page(pageNumber,limit,totalCount);
+				String limitSql = dialect.getLimitString(sql,page.getFirstResult(),limit);
+				List list = getNamedParameterJdbcTemplate().query(limitSql, paramMap, new BeanPropertyRowMapper(getEntityClass()));
+				page.setThisPageElements(list);
+				return page;
+			}else {
+				String limitSql = dialect.getLimitString(sql, 0, limit);
+				return getJdbcScrollPage(pageNumber,limit, limitSql,paramMap,totalCount);
+			}
+		}else {
+			return getJdbcScrollPage(pageNumber,limit, sql,paramMap,totalCount);			
+		}
+	}
+
+	private Page getJdbcScrollPage(final int pageNumber,final int pageSize,
+			String sql,Map filters, final int totalCount) {
+		return (Page)getNamedParameterJdbcTemplate().execute(sql, filters, new PreparedStatementCallback() {
+			public Object doInPreparedStatement(PreparedStatement ps)
+					throws SQLException, DataAccessException {
+				ps.setMaxRows(pageSize);
+				ResultSet rs = ps.executeQuery();
+				return new JdbcScrollPage(rs,totalCount,new BeanPropertyRowMapper(getEntityClass()),pageNumber,pageSize);
+			}
+		});
+	}
+	
+	private void setIdentifierProperty(Object entity, Object id) {
+		try {
+			BeanUtils.setProperty(entity, getIdentifierPropertyName(), convert2CorrectIdType(id));
+		} catch (Exception e) {
+			ReflectionUtils.handleReflectionException(e);
+		}
+	}
+
+	protected void insertWithIdentity(Object entity,String insertSql) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		getNamedParameterJdbcTemplate().update(insertSql, new BeanPropertySqlParameterSource(entity) , keyHolder);
+		setIdentifierProperty(entity, keyHolder.getKey().longValue());
+	}
+
+	protected void insertWithAutoIncrement(Object entity,String insertSql) {
+		insertWithIdentity(entity,insertSql);
+	}
+	
+	protected void insertWithSequence(Object entity,AbstractSequenceMaxValueIncrementer sequenceIncrementer,String insertSql) {
+		Long id = sequenceIncrementer.nextLongValue();
+		setIdentifierProperty(entity, id);
+		getNamedParameterJdbcTemplate().update(insertSql, new BeanPropertySqlParameterSource(entity));
+	}
+	
+	protected void insertWithDB2Sequence(Object entity,String sequenceName,String insertSql) {
+		insertWithSequence(entity, new DB2SequenceMaxValueIncrementer(getDataSource(),sequenceName), insertSql);
+	}
+	
+	protected void insertWithOracleSequence(Object entity,String sequenceName,String insertSql) {
+		insertWithSequence(entity, new OracleSequenceMaxValueIncrementer(getDataSource(),sequenceName), insertSql);
+	}
+	
+	protected void insertWithUUID(Object entity,String insertSql) {
+		String uuid = UUID.randomUUID().toString().replace("-", "");
+		setIdentifierProperty(entity, uuid);
+		getNamedParameterJdbcTemplate().update(insertSql, new BeanPropertySqlParameterSource(entity));
+	}
+	
+	protected void insertWithAssigned(Object entity,String insertSql) {
+		getNamedParameterJdbcTemplate().update(insertSql, new BeanPropertySqlParameterSource(entity));
+	}
+	
+	public void flush() {
+		//ignore
+	}
+	
+	public void saveOrUpdate(Object entity) {
+		Object id = getIdentifierPropertyValue(entity);
+		if(ObjectUtils.isNullOrEmptyString(id)) {
+			save(entity);
+		}else {
+			update(entity);
+		}
+	}
+
+}
---
+++ trunk/rapid-framework/plugins/spring_jdbc/template/java_src/${basepackage_dir}/${subpackage}/dao/${className}Dao.java
@@ -0,0 +1,83 @@
+<#include "/java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first>   
+package ${basepackage}.${subpackage}.dao;
+
+<#include "/java_imports.include">
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.registry.infomodel.User;
+
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ${className}Dao extends BaseSpringJdbcDao{
+
+	public Class getEntityClass() {
+		return ${className}.class;
+	}
+	public String getIdentifierPropertyName() {
+		<#if table.singleId>
+		return "${table.idColumn.columnNameLower}";
+		<#else>
+		return null;
+		</#if>
+	}
+	
+	public void deleteById(Serializable id) {
+		getSimpleJdbcTemplate().update("delete from ${table.sqlName} where ${table.idColumn.sqlName}=?", id);
+	}
+	
+	public List findAll() {
+		String sql = "select <#list table.columns as column>${column.sqlName}<#if column_has_next>,</#if></#list> from ${table.sqlName}";
+		return getSimpleJdbcTemplate().query(sql, ParameterizedBeanPropertyRowMapper.newInstance(getEntityClass()));
+	}
+
+	public ${className} getById(Serializable id) {
+		String sql = "select <#list table.columns as column>${column.sqlName}<#if column_has_next>,</#if></#list> from ${table.sqlName} where ${table.idColumn.sqlName}=? ";
+		return getSimpleJdbcTemplate().queryForObject(sql, ParameterizedBeanPropertyRowMapper.newInstance(${className}.class), id);
+	}
+
+	public void save(Object entity) {
+		String sql = "insert into ${table.sqlName} " 
+			 + " (<#list table.columns as column>${column.sqlName}<#if column_has_next>,</#if></#list>) " 
+			 + " values(<#list table.columns as column>:${column.columnNameLower}<#if column_has_next>,</#if></#list>)";
+		insertWithIdentity(entity,sql);
+	}
+	
+	public void update(Object entity) {
+		String sql = "update ${table.sqlName} set <#list table.columns as column>${column.sqlName}=:${column.columnNameLower}<#if column_has_next>,</#if></#list> "
+					+ " where ${table.idColumn.sqlName}=:${table.idColumn.columnNameLower}";
+		getNamedParameterJdbcTemplate().update(sql, new BeanPropertySqlParameterSource(entity));
+	}
+	
+	public Page findByPageRequest(PageRequest pageRequest) {
+		//XsqlBuilder syntax,please see http://code.google.com/p/rapid-xsqlbuilder
+		String sql = "select <#list table.columns as column>${column.sqlName}<#if column_has_next>,</#if></#list> from ${table.sqlName} as a where 1=1 "
+			<#list table.columns as column>
+			  	<#if column.isNotIdOrVersionField>
+				+ "/~ and a.${column.sqlName} = '[${column.columnNameLower}]' ~/"
+				</#if>
+			</#list>
+				+ "/~ order by [sortingColumn] [sortingDirection] ~/";
+		return pageQuery(sql,pageRequest);
+	}
+	
+	public boolean isUnique(Object entity, String uniquePropertyNames) {
+		return false;
+	}
+	
+	<#list table.columns as column>
+	<#if column.unique && !column.pk>
+	public ${className} getBy${column.columnName}(${column.javaType} v) {
+		String sql = "select <#list table.columns as column>${column.sqlName}<#if column_has_next>,</#if></#list> from ${table.sqlName} where ${column.columnNameLower}=?";
+		return (${className})getSimpleJdbcTemplate().queryForObject(sql, ParameterizedBeanPropertyRowMapper.newInstance(User.class), v);
+	}	
+	</#if>
+	</#list>
+
+}
