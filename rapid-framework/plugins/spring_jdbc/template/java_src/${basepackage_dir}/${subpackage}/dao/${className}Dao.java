a2a141f83184bdf601f69c16f80f344efb7406cb
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-06-30 07:09:46 +0000

    修正oracle语法错误

--- rapid-framework/plugins/spring_jdbc/template/java_src/${basepackage_dir}/${subpackage}/dao/${className}Dao.java
+++ rapid-framework/plugins/spring_jdbc/template/java_src/${basepackage_dir}/${subpackage}/dao/${className}Dao.java
@@ -73,7 +73,7 @@ public class ${className}Dao extends BaseSpringJdbcDao<${className},${table.idCo
 		//XsqlBuilder syntax,please see http://code.google.com/p/rapid-xsqlbuilder
 		// $column$为字符串拼接, #column#为使用占位符. 以下为图方便采用sql拼接,适用性能要求不高的应用,使用占位符方式可以优化性能. 
 		// $column$ 为PageRequest.getFilters()中的key
-		String sql = SELECT_PREFIX + " as a where 1=1 "
+		String sql = SELECT_PREFIX + " a where 1=1 "
 			<#list table.columns as column>
 			  	<#if column.isNotIdOrVersionField>
 				+ "/~ and a.${column.sqlName} = '$${column.columnNameLower}$' ~/"
