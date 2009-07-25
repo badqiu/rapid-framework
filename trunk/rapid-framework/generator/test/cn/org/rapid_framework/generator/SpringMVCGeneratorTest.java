6f0c1589a1a51cd6171fdc00ba3f9c1687b466f0
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-07-25 04:28:55 +0000

    generator test

---
+++ trunk/rapid-framework/generator/test/cn/org/rapid_framework/generator/SpringMVCGeneratorTest.java
@@ -0,0 +1,26 @@
+package cn.org.rapid_framework.generator;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+import cn.org.rapid_framework.generator.provider.db.DbTableFactory;
+import cn.org.rapid_framework.generator.provider.db.DbTableGeneratorModelProvider;
+import cn.org.rapid_framework.generator.provider.db.model.Table;
+
+public class SpringMVCGeneratorTest extends TestCase{
+
+	public void testGenerate() throws Exception{
+		Generator g = new Generator();
+		g.outRootDir = ".";
+		
+		Table table = DbTableFactory.getInstance().getTable("blog");
+		
+		g.addTemplateRootDir(new File("template").getAbsoluteFile());
+		g.addTemplateRootDir(new File("plugins/springmvc/template"));
+		
+		g.generateByModelProvider(new DbTableGeneratorModelProvider(table));
+		
+//		Runtime.getRuntime().exec("cmd.exe /c start "+new File(g.outRootDir).getAbsolutePath());
+	}
+	
+}
