16e215f75466844e3fc7f55dca9032c9b8134265
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-07-26 11:02:00 +0000

    revert MapAndObject

---
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/MapAndObject.java
@@ -0,0 +1,123 @@
+package cn.org.rapid_framework.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
+import java.util.regex.Pattern;
+
+import org.apache.commons.beanutils.PropertyUtils;
+/**
+ * 用于get(key)方法对Map及Bean混合取值.
+ * 如果从map中取值为null,则会从bean中取值
+ * @author badqiu
+ */
+@SuppressWarnings("unchecked")
+public class MapAndObject implements Map{
+	Map map;
+	Object bean;
+
+	public MapAndObject(Map map, Object bean) {
+		super();
+		this.map = map;
+		this.bean = bean;
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public Object getBean() {
+		return bean;
+	}
+	
+	public Object get(Object key) {
+		return getProperty(key);
+	}
+
+	static Pattern ERROR_METHOD_PATTERN = Pattern.compile("^[^a-zA-Z_].*");
+	static Map<String,String> noSuchMethodCache = Collections.synchronizedMap(new WeakHashMap());
+	Object getProperty(Object key) {
+		Object result = null;
+		if (map != null) {
+			result = map.get(key);
+		}
+		
+		if(result == null && bean != null && bean instanceof Map) {
+			return ((Map)bean).get(key);
+		}
+		
+		if (result == null && bean != null && key instanceof String) {
+			String prop = (String)key;
+			String classMethod = bean.getClass().getName()+"."+prop;
+			if(noSuchMethodCache.containsKey(classMethod)) {
+				return null;
+			}
+			if(ERROR_METHOD_PATTERN.matcher(prop).matches()) {
+				return null;
+			}
+			
+			try {
+				result = PropertyUtils.getProperty(bean, prop);
+			} catch (IllegalAccessException e) {
+				throw new IllegalStateException(
+						"cannot get property value by property:" + key
+								+ " on class:" + bean.getClass(), e);
+			} catch (InvocationTargetException e) {
+				throw new IllegalStateException(
+						"cannot get property value by property:" + key
+								+ " on class:" + bean.getClass(), e);
+			} catch (NoSuchMethodException e) {
+				noSuchMethodCache.put(classMethod, null);
+				return null;
+			}
+		}
+		return result;
+	}
+
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean containsKey(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean containsValue(Object value) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Set entrySet() {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean isEmpty() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Set keySet() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Object put(Object key, Object value) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void putAll(Map m) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Object remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int size() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Collection values() {
+		throw new UnsupportedOperationException();
+	}
+}
