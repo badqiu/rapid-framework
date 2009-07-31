25aa10543674b5a20ce97a2a38c166f6c3fc9abf
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-07-31 14:14:28 +0000

    add comments

--- trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/IGeneratorModelProvider.java
+++ trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/IGeneratorModelProvider.java
@@ -2,7 +2,7 @@ package cn.org.rapid_framework.generator;
 
 import java.util.Map;
 /**
- * 
+ * 该接口用于为 模板及路径提供相关变量的引用
  * @author badqiu
  *
  */
@@ -10,8 +10,10 @@ public interface IGeneratorModelProvider {
 
 	public String getDisaplyText();
 	
+	/** 得到文件路径可以引用的变量 */
 	public void mergeTemplateModel(Map model) throws Exception;
 	
+	/** 得到模板文件可以引用的变量 */
 	public void mergeFilePathModel(Map model) throws Exception;
 	
 }
