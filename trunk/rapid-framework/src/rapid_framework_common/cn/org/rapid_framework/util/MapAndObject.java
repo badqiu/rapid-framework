1bbebeeddf4b3ffdc07c2916b227f2c7bf3d0189
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-08-08 00:45:52 +0000

    重构并使用spring BeanUtils

--- trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/MapAndObject.java
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/MapAndObject.java
@@ -15,6 +15,9 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
 /**
  * 混合两个对象为一个Map对象
  * MapAndObject.get()方法将在两个对象取值,Map如果取值为null,则再在Bean中取值
@@ -133,90 +136,15 @@ public class MapAndObject implements Map{
 						"cannot get property value by property:" + propertyName + " on class:" + bean.getClass(), e);
 			} catch (InvocationTargetException e) {
 				throw new IllegalStateException(
-						"cannot get property value by property:" + propertyName + " on class:" + bean.getClass(), e);
-			} catch (IntrospectionException e) {
-				throw new IllegalStateException(
-						"cannot get property value by property:" + propertyName + " on class:" + bean.getClass(), e);
+						"cannot get property value by property:" + propertyName + " on class:" + bean.getClass(), e.getTargetException());
 			}
 		}
 
-		private static Method getReadMethod(Object bean, String propertyName) throws IntrospectionException {
-			ExtendBeanInfo beanInfo = getBeanInfo(bean.getClass());
-			PropertyDescriptor pd = beanInfo.getPropertyDescriptor(propertyName);
+		private static Method getReadMethod(Object bean, String propertyName) {
+			PropertyDescriptor pd = BeanUtils.getPropertyDescriptor(bean.getClass(), propertyName);
 			if(pd == null) return null;
 			return pd.getReadMethod();
 		}
 		
-		private static Map beanInfoCache = Collections.synchronizedMap(new WeakHashMap());
-		private static ExtendBeanInfo getBeanInfo(Class clazz) throws IntrospectionException {
-			ExtendBeanInfo beanInfo = (ExtendBeanInfo)beanInfoCache.get(clazz);
-			if(beanInfo == null) {
-				beanInfo = new ExtendBeanInfo(Introspector.getBeanInfo(clazz));
-				beanInfoCache.put(clazz, beanInfo);
-			}
-			return beanInfo;
-		}
-		
-		private static class ExtendBeanInfo implements BeanInfo {
-			BeanInfo delegate;
-			private Map propertyDescriptorCache = Collections.synchronizedMap(new WeakHashMap());
-			public ExtendBeanInfo(BeanInfo delegate) {
-				super();
-				this.delegate = delegate;
-			}
-	
-			public BeanInfo[] getAdditionalBeanInfo() {
-				return delegate.getAdditionalBeanInfo();
-			}
-	
-			public BeanDescriptor getBeanDescriptor() {
-				return delegate.getBeanDescriptor();
-			}
-	
-			public int getDefaultEventIndex() {
-				return delegate.getDefaultEventIndex();
-			}
-	
-			public int getDefaultPropertyIndex() {
-				return delegate.getDefaultPropertyIndex();
-			}
-	
-			public EventSetDescriptor[] getEventSetDescriptors() {
-				return delegate.getEventSetDescriptors();
-			}
-	
-			public Image getIcon(int iconKind) {
-				return delegate.getIcon(iconKind);
-			}
-	
-			public MethodDescriptor[] getMethodDescriptors() {
-				return delegate.getMethodDescriptors();
-			}
-	
-			public PropertyDescriptor[] getPropertyDescriptors() {
-				return delegate.getPropertyDescriptors();
-			}
-			
-			public PropertyDescriptor getPropertyDescriptor(String name) {
-				if(propertyDescriptorCache.containsKey(name)) {
-					return (PropertyDescriptor)propertyDescriptorCache.get(name);
-				}
-				PropertyDescriptor pd = getPropertyDescriptorFromArray(name);
-				propertyDescriptorCache.put(name, pd);
-				return pd;
-			}
-	
-			private PropertyDescriptor getPropertyDescriptorFromArray(String name) {
-				PropertyDescriptor pd = null;
-				PropertyDescriptor[] propertyDescriptors = getPropertyDescriptors();
-				for(int i = 0; i < propertyDescriptors.length; i++) {
-					PropertyDescriptor temp = propertyDescriptors[i];
-					if(temp.getName().equals(name)) {
-						pd = propertyDescriptors[i];
-					}
-				}
-				return pd;
-			}
-		}
 	}
 }
