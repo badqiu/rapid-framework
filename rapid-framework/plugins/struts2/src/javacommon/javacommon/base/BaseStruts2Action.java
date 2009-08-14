aa95a2f5fed309d717e6ae454d141e3fccd17944
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-08-14 14:07:46 +0000

    savePage(page) => savePage(page,pageRequest)

--- rapid-framework/plugins/struts2/src/javacommon/javacommon/base/BaseStruts2Action.java
+++ rapid-framework/plugins/struts2/src/javacommon/javacommon/base/BaseStruts2Action.java
@@ -1,41 +1,23 @@
 package javacommon.base;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import javacommon.util.ConvertRegisterHelper;
 import javacommon.util.PageRequestFactory;
 
-import org.apache.commons.beanutils.ConvertUtils;
-import org.apache.commons.beanutils.converters.BigDecimalConverter;
-import org.apache.commons.beanutils.converters.BigIntegerConverter;
-import org.apache.commons.beanutils.converters.DoubleConverter;
-import org.apache.commons.beanutils.converters.FloatConverter;
-import org.apache.commons.beanutils.converters.IntegerConverter;
-import org.apache.commons.beanutils.converters.LongConverter;
-import org.apache.commons.beanutils.converters.SqlDateConverter;
-import org.apache.commons.beanutils.converters.SqlTimeConverter;
-import org.apache.commons.beanutils.converters.SqlTimestampConverter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.RequestAware;
-import org.apache.struts2.interceptor.SessionAware;
 import org.springframework.util.Assert;
 
 import cn.org.rapid_framework.beanutils.BeanUtils;
-import cn.org.rapid_framework.beanutils.converter.StringConverter;
 import cn.org.rapid_framework.page.Page;
 import cn.org.rapid_framework.page.PageRequest;
 import cn.org.rapid_framework.util.ObjectUtils;
 
 import com.opensymphony.xwork2.ActionSupport;
-import com.opensymphony.xwork2.ModelDriven;
 
 public abstract class BaseStruts2Action extends ActionSupport implements RequestAware {
 	protected Map requestMap = null;
@@ -57,18 +39,22 @@ public abstract class BaseStruts2Action extends ActionSupport implements Request
 		this.requestMap = request;
 	}
 
-	public void savePage(Page page){
-		savePage("",page);
+	public void savePage(Page page,PageRequest pageRequest){
+		savePage("",page,pageRequest);
 	}
 	
 	/**
 	 * 用于一个页面有多个extremeTable是使用
 	 * @param tableId 等于extremeTable的tableId属性
 	 */
-	public void savePage(String tableId,Page page){
-		Assert.notNull(tableId);
-		requestMap.put(tableId+"page", page);
-		requestMap.put(tableId+"totalRows", new Integer(page.getTotalCount()));
+	public void savePage(String tableId,Page page,PageRequest pageRequest){
+		Assert.notNull(tableId,"tableId must be not null");
+		Assert.notNull(page,"page must be not null");
+		Assert.notNull(pageRequest,"pageRequest must be not null");
+		
+		getRequest().setAttribute(tableId+"page", page);
+		getRequest().setAttribute(tableId+"totalRows", new Integer(page.getTotalCount()));
+		getRequest().setAttribute(tableId+"pageRequest", pageRequest);
 	}
 	
 	public PageRequest newPageRequest(String defaultSortColumns){
@@ -86,5 +72,6 @@ public abstract class BaseStruts2Action extends ActionSupport implements Request
 	public HttpServletResponse getResponse() {
 		return ServletActionContext.getResponse();
 	}
+	
 }
 
