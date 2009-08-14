2e0a2d28dc69177e4c8df2691183c227819bd5ec
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-08-14 09:54:58 +0000

    add author

--- rapid-framework/plugins/simpletable/src/javacommon/javacommon/util/SimpleTablePageHelper.java
+++ rapid-framework/plugins/simpletable/src/javacommon/javacommon/util/SimpleTablePageHelper.java
@@ -10,10 +10,18 @@ import org.springframework.web.util.WebUtils;
 
 import cn.org.rapid_framework.page.PageRequest;
 
+/**
+ * @author badqiu
+ */
 public class SimpleTablePageHelper {
 	
+	private static final int MAX_PAGE_SIZE = 500;
 	private static final int DEFAULT_PAGE_SIZE = 10;
-
+	static {
+		System.out.println("SimpleTablePageHelper.DEFAULT_PAGE_SIZE="+DEFAULT_PAGE_SIZE);
+		System.out.println("SimpleTablePageHelper.MAX_PAGE_SIZE="+MAX_PAGE_SIZE);
+	}
+	
 	public static PageRequest<Map> newPageRequest(HttpServletRequest request,String defaultSortColumn) {
 		return newPageRequest(request, DEFAULT_PAGE_SIZE,defaultSortColumn);
 	}
@@ -24,6 +32,10 @@ public class SimpleTablePageHelper {
 		result.setPageSize(ServletRequestUtils.getIntParameter(request, "pageSize", defaultPageSize));
 		result.setSortColumns(ServletRequestUtils.getStringParameter(request, "sortColumns",defaultSortColumn));
 		result.setFilters(WebUtils.getParametersStartingWith(request,"auto_include_"));
+		
+		if(result.getPageSize() > MAX_PAGE_SIZE) {
+			result.setPageSize(MAX_PAGE_SIZE);
+		}
 		return result;
 	}
 	
