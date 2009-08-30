f2898239e196d7164a7f12a7355687f68524e673
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-08-30 15:51:32 +0000

    simpletable 分页

--- trunk/rapid-framework/plugins/simpletable/src/javacommon/javacommon/util/SimpleTablePageRequestFactory.java
+++ trunk/rapid-framework/plugins/simpletable/src/javacommon/javacommon/util/SimpleTablePageRequestFactory.java
@@ -21,15 +21,15 @@ public class SimpleTablePageRequestFactory {
 		System.out.println("SimpleTablePageRequestFactory.MAX_PAGE_SIZE="+MAX_PAGE_SIZE);
 	}
 	
-	public static PageRequest<Map> newPageRequest(HttpServletRequest request,String defaultSortColumn) {
-		return newPageRequest(request,defaultSortColumn,DEFAULT_PAGE_SIZE);
+	public static PageRequest<Map> newPageRequest(HttpServletRequest request,String defaultSortColumns) {
+		return newPageRequest(request,defaultSortColumns,DEFAULT_PAGE_SIZE);
 	}
 	
-	public static PageRequest<Map> newPageRequest(HttpServletRequest request,String defaultSortColumn,int defaultPageSize) {
+	public static PageRequest<Map> newPageRequest(HttpServletRequest request,String defaultSortColumns,int defaultPageSize) {
 		PageRequest<Map> result = new PageRequest();
 		result.setPageNumber(getIntParameter(request, "pageNumber", 1));
 		result.setPageSize(getIntParameter(request, "pageSize", defaultPageSize));
-		result.setSortColumns(getStringParameter(request, "sortColumns",defaultSortColumn));
+		result.setSortColumns(getStringParameter(request, "sortColumns",defaultSortColumns));
 		result.setFilters(WebUtils.getParametersStartingWith(request,"auto_include_"));
 		
 		if(result.getPageSize() > MAX_PAGE_SIZE) {
