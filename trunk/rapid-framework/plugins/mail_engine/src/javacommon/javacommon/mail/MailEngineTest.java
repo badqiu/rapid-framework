4743f328b380fea52f561a36b5d704611877ae6c
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-16 09:16:33 +0000

    mail_engine plugin init imports.

---
+++ trunk/rapid-framework/plugins/mail_engine/src/javacommon/javacommon/mail/MailEngineTest.java
@@ -0,0 +1,49 @@
+package javacommon.mail;
+
+import junit.framework.TestCase;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+
+import cn.org.rapid_framework.util.concurrent.async.AsyncToken;
+import cn.org.rapid_framework.util.concurrent.async.IResponder;
+
+public class MailEngineTest extends TestCase {
+	MailEngine engine = new MailEngine();
+
+	
+	
+	public void setUp()throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext-mail.xml");
+		engine = (MailEngine)context.getBean("mailEngine");
+	}
+	
+	public void tearDown() throws Exception{
+		engine.destroy();
+	}
+	
+	public void testSend() throws Exception {
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setTo("qiubaichao@kingsoft.com");
+		msg.setFrom("rapidframework@126.com");
+		msg.setText("hello: badqiu, <h1>2008</h1>");
+		msg.setSubject("test subject");
+		
+		AsyncToken token = engine.sendAsyncMsg(msg);
+		
+		token.addResponder(new IResponder() {
+			public void onFault(Exception e) {
+				System.out.println("send email fail,cause:"+e);
+				e.printStackTrace();
+			}
+
+			public void onResult(Object result) {
+				System.out.println("send email success");
+			}
+		});
+		
+		Thread.sleep(1000 * 5);
+	}
+	
+}
