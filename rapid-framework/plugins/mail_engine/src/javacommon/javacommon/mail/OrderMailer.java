1a42c138e16bd475a3065d0cf3f22c0cecfba543
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-16 16:06:07 +0000

    使用AsyncJavaMailSender

--- rapid-framework/plugins/mail_engine/src/javacommon/javacommon/mail/OrderMailer.java
+++ rapid-framework/plugins/mail_engine/src/javacommon/javacommon/mail/OrderMailer.java
@@ -6,8 +6,11 @@ import org.springframework.mail.SimpleMailMessage;
 
 public class OrderMailer {
 	
-	public SimpleMailMessage createConfirmOrder(Map order) {
+	public static SimpleMailMessage createConfirmOrder(Map order) {
 		return null;
 	}
 	
+	public static void sendConfirmOrder(Map order) {
+		SimpleMailMessage msg = createConfirmOrder(order);
+	}
 }
