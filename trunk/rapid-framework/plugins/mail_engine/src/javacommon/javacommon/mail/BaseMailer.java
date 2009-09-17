48838f9229817933559dcf74dd135df868c3b10e
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-17 03:43:25 +0000

    完善mailer

--- trunk/rapid-framework/plugins/mail_engine/src/javacommon/javacommon/mail/BaseMailer.java
+++ trunk/rapid-framework/plugins/mail_engine/src/javacommon/javacommon/mail/BaseMailer.java
@@ -19,12 +19,12 @@ import freemarker.template.TemplateException;
  * 现本mailer与freemarker模板结合,用于生成邮件内容
  * 本类你也可以自由修改
  * 
- * Mailer使用示例:
+ * Mailer使用规范:
  * 1. 包名: 放在mailer包内,如com.company.project.mailer
  * 2. 类名: 以Mailer结尾,如UserMailer
  * 3. 方法名: 
- * 			使用UserMailer.createXXXX()来创建邮件消息,如UserMailer.createNotifaction()
- * 			使用UserMailer.sendXXXX()来发送邮件,如UserMailer.sendNotifaction()
+ * 			使用UserMailer.createXXXX()来创建邮件消息,如UserMailer.createConfirmMail()
+ * 			使用UserMailer.sendXXXX()来发送邮件,如UserMailer.sendConfirmMail()
  * 4.必须继承之BaseMailer
  * 
  * @author badqiu
