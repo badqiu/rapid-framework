397366e2a31bfaef3f088a2b0c80a52393fc5aea
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-18 16:07:54 +0000

    重构

---
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/concurrent/async/AsyncTokenUtils.java
@@ -0,0 +1,45 @@
+package cn.org.rapid_framework.util.concurrent.async;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+
+/**
+ * AsyncToken工具类方法,用executor执行任务并设置结果用于自动生成AsyncToken
+ * <pre>
+ * 
+ * </pre>
+ * @author badqiu
+ *
+ */
+public class AsyncTokenUtils {
+
+	public static <T> AsyncToken<T> execute(Executor executor,final Callable<T> task) {
+		final AsyncToken token = new AsyncToken();
+		executor.execute(new Runnable() {
+			public void run() {
+				try {
+					token.setComplete(task.call());
+				} catch (Exception e) {
+					token.setComplete();
+				}
+			}
+		});
+		return token;
+	}
+	
+	public static <T> AsyncToken<T> execute(Executor executor,final Runnable task) {
+		final AsyncToken token = new AsyncToken();
+		executor.execute(new Runnable() {
+			public void run() {
+				try {
+					task.run();
+					token.setComplete();
+				}catch(Exception e) {
+					token.setFault(e);
+				}
+			}
+		});
+		return token;
+	}
+	
+}
