28a16149f043e08e1e3b42186dfe7e0fff0776c9
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-18 17:55:47 +0000

    重构

--- trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/concurrent/async/AsyncTokenUtils.java
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/concurrent/async/AsyncTokenUtils.java
@@ -39,42 +39,4 @@ public class AsyncTokenUtils {
 		return token;
 	}
 	
-	public static class AsyncTokenRunnable implements Runnable {
-		AsyncToken asyncToken ;
-		Runnable targetRunnable;
-		Callable targetCallable;
-		
-		public AsyncTokenRunnable(AsyncToken asyncToken,Runnable target) {
-			if(asyncToken == null) throw new IllegalArgumentException("asyncToken must be not null");
-			if(target == null) throw new IllegalArgumentException("target Runnable must be not null");
-			this.asyncToken = asyncToken;
-			this.targetRunnable = target;
-		}
-		
-		public AsyncTokenRunnable(AsyncToken asyncToken,Callable target) {
-			if(asyncToken == null) throw new IllegalArgumentException("asyncToken must be not null");
-			if(target == null) throw new IllegalArgumentException("target Callable must be not null");
-			this.asyncToken = asyncToken;
-			this.targetCallable = target;
-		}
-
-		public AsyncToken getAsyncToken() {
-			return asyncToken;
-		}
-
-		public void run() {
-			try {
-				if(targetRunnable == null) {
-					asyncToken.setComplete(targetCallable.call());
-				}else {
-					targetRunnable.run();
-					asyncToken.setComplete();
-				}
-			}catch(Exception e) {
-				asyncToken.setFault(e);
-			}
-		}
-
-	}
-	
 }
