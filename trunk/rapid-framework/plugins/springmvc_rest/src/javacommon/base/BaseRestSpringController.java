72f53cb72069afd4d0818b0dd2a698abf2a3a053
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-21 17:15:42 +0000

    完成springmvc rest插件

---
+++ trunk/rapid-framework/plugins/springmvc_rest/src/javacommon/base/BaseRestSpringController.java
@@ -0,0 +1,81 @@
+package javacommon.base;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 定义标准的rest方法以对应实体对象的操作,以达到统一rest的方法名称,
+ * 还可以避免子类需要重复编写@RequestMapping annotation.
+ * 
+ * 子类要实现某功能只需覆盖下面的方法即可.
+ * 注意: 覆盖时请使用@Override,以避免发生错误
+ * <pre>
+ * /userinfo                => index()  
+ * /userinfo/new            => _new()  
+ * /userinfo/{id}           => show()  
+ * /userinfo/{id}/edit      => edit()  
+ * /userinfo        POST    => create()  
+ * /userinfo/{id}   PUT     => update()  
+ * /userinfo/{id}   DELETE  => delete()  
+ * /userinfo        DELETE  => batchDelete()  
+ * </pre>
+ * @author badqiu
+ * @param <T> 实体对象,如Blog
+ */
+public class BaseRestSpringController<Entity,PK> extends BaseSpringController{
+	
+	@RequestMapping
+	public ModelAndView index(HttpServletRequest request,HttpServletResponse response,Entity model) {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 进入新增 */
+	@RequestMapping(value="/new")
+	public ModelAndView _new(HttpServletRequest request,HttpServletResponse response,Entity model) throws Exception {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 显示 */
+	@RequestMapping(value="/{id}")
+	public ModelAndView show(@PathVariable PK id) throws Exception {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 编辑 */
+	@RequestMapping(value="/{id}/edit")
+	public ModelAndView edit(@PathVariable PK id) throws Exception {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 保存新增 */
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView create(HttpServletRequest request,HttpServletResponse response,Entity model) throws Exception {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 保存更新 */
+	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
+	public ModelAndView update(@PathVariable PK id,HttpServletRequest request,HttpServletResponse response) throws Exception {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+	
+	/** 删除 */
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ModelAndView delete(@PathVariable PK id) {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+
+	/** 批量删除 */
+	@RequestMapping(method=RequestMethod.DELETE)
+	public ModelAndView batchDelete(@RequestParam("items") PK[] items) {
+		throw new UnsupportedOperationException("not yet implement");
+	}
+}
--- trunk/rapid-framework/plugins/springmvc_rest/template/web/pages/${classNameLowerCase}/edit.jsp
+++ trunk/rapid-framework/plugins/springmvc_rest/template/web/pages/${classNameLowerCase}/edit.jsp
@@ -1,3 +1,8 @@
+<#include "/macro.include"/>
+<#include "/custom.include"/>  
+<#assign className = table.className>   
+<#assign classNameFirstLower = className?uncap_first> 
+<#assign classNameLowerCase = className?lower_case> 
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ include file="/commons/taglibs.jsp" %>
 <%
@@ -9,15 +14,15 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 <head>
 	<%@ include file="/commons/meta.jsp" %>
 	<base href="<%=basePath%>">
-	<title><%=UserInfo.TABLE_ALIAS%>编辑</title>
+	<title><%=${className}.TABLE_ALIAS%>编辑</title>
 </head>
 
 <body>
 <%@ include file="/commons/messages.jsp" %>
 
-<form:form method="put" action="${ctx}/userinfo/${userInfo.userId}" modelAttribute="userInfo">
+<form:form method="put" action="<@jspEl 'ctx'/>/${classNameLowerCase}/<@jspEl classNameFirstLower+'.'+table.idColumn.columnNameFirstLower/>" modelAttribute="${classNameFirstLower}">
 	<input id="submitButton" name="submitButton" type="submit" value="提交" />
-	<input type="button" value="返回列表" onclick="window.location='${ctx}/userinfo'"/>
+	<input type="button" value="返回列表" onclick="window.location='<@jspEl 'ctx'/>/userinfo'"/>
 	<input type="button" value="后退" onclick="history.back();"/>
 	
 	<table class="formTable">
