49ad8f5a51d0ae29af7a669358376c4930aa2f26
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-24 16:45:48 +0000

    delete @TODO

--- rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/model/ForeignKey.java
+++ rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/model/ForeignKey.java
@@ -1,8 +1,6 @@
 /*
  * Created on Jan 1, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package cn.org.rapid_framework.generator.provider.db.model;
 
@@ -15,8 +13,6 @@ import cn.org.rapid_framework.generator.util.ListHashtable;
 /**
  * @author chris
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ForeignKey {
 
--- rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/model/ForeignKeys.java
+++ rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/model/ForeignKeys.java
@@ -1,8 +1,6 @@
 /*
  * Created on Jan 6, 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package cn.org.rapid_framework.generator.provider.db.model;
 
