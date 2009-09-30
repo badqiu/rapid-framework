650dcca31e55766141e31ac6d1198c24f5995007
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-09-30 01:57:36 +0000

    fixed filters error

--- trunk/rapid-framework/plugins/ibatis3/src/javacommon/javacommon/base/BaseIbatis3Dao.java
+++ trunk/rapid-framework/plugins/ibatis3/src/javacommon/javacommon/base/BaseIbatis3Dao.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-
-import javacommon.base.EntityDao;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,7 +15,6 @@ import org.springframework.util.Assert;
 import cn.org.rapid_framework.beanutils.BeanUtils;
 import cn.org.rapid_framework.page.Page;
 import cn.org.rapid_framework.page.PageRequest;
-import cn.org.rapid_framework.util.MapAndObject;
 
 /**
  * @author badqiu
@@ -100,17 +96,21 @@ public abstract class BaseIbatis3Dao<E,PK extends Serializable> extends DaoSuppo
 		Page page = new Page(pageRequest,totalCount.intValue());
 		
 		//其它分页参数,用于不喜欢或是因为兼容性而不使用方言(Dialect)的分页用户使用. 与getSqlMapClientTemplate().queryForList(statementName, parameterObject)配合使用
-		Map otherFilters = new HashMap();
-		otherFilters.put("offset", page.getFirstResult());
-		otherFilters.put("pageSize", page.getPageSize());
-		otherFilters.put("lastRows", page.getFirstResult() + page.getPageSize());
-		otherFilters.put("sortColumns", pageRequest.getSortColumns());
+		Map filters = new HashMap();
+		filters.put("offset", page.getFirstResult());
+		filters.put("pageSize", page.getPageSize());
+		filters.put("lastRows", page.getFirstResult() + page.getPageSize());
+		filters.put("sortColumns", pageRequest.getSortColumns());
 		
 		//混合两个filters为一个filters,MapAndObject.get()方法将在两个对象取值,Map如果取值为null,则再在Bean中取值
-		Map parameterObject = BeanUtils.describe(pageRequest.getFilters());
-		otherFilters.putAll(parameterObject);
+		if(pageRequest.getFilters() instanceof Map) {
+			filters.putAll((Map)pageRequest.getFilters());
+		}else {
+			Map parameterObject = BeanUtils.describe(pageRequest.getFilters());
+			filters.putAll(parameterObject);
+		}
 		
-		List list = getSqlSessionTemplate().selectList(statementName, otherFilters,page.getFirstResult(),page.getPageSize());
+		List list = getSqlSessionTemplate().selectList(statementName, filters,page.getFirstResult(),page.getPageSize());
 		page.setResult(list);
 		return page;
 	}
