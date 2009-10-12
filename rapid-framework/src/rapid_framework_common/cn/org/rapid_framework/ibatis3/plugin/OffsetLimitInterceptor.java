a36693fa10e480be48341891a22946a090cdb3b7
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-10-12 06:42:37 +0000

    升级至iBatis3 beta4

--- rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/ibatis3/plugin/OffsetLimitInterceptor.java
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/ibatis3/plugin/OffsetLimitInterceptor.java
@@ -5,7 +5,6 @@ package cn.org.rapid_framework.ibatis3.plugin;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.result.ResultHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
@@ -15,10 +14,10 @@ import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.util.ReflectionUtils;
 
-import cn.org.rapid_framework.ibatis3.plugin.OffsetLimitInterceptor.BoundSqlSqlSource;
 import cn.org.rapid_framework.jdbc.dialect.Dialect;
 import cn.org.rapid_framework.util.PropertiesHelper;
 
