28e0bbb26b6d8dce1a658b47d819a8db95b17418
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-10-12 06:38:20 +0000

    升级至iBatis3 beta4

--- rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/ibatis3/plugin/OffsetLimitInterceptor.java
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/ibatis3/plugin/OffsetLimitInterceptor.java
@@ -15,8 +15,10 @@ import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.util.ReflectionUtils;
 
+import cn.org.rapid_framework.ibatis3.plugin.OffsetLimitInterceptor.BoundSqlSqlSource;
 import cn.org.rapid_framework.jdbc.dialect.Dialect;
 import cn.org.rapid_framework.util.PropertiesHelper;
 
@@ -41,13 +43,12 @@ import cn.org.rapid_framework.util.PropertiesHelper;
 @Intercepts({@Signature(
 		type= Executor.class,
 		method = "query",
-		args = {MappedStatement.class, Object.class, int.class, int.class, ResultHandler.class})})
+		args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class OffsetLimitInterceptor implements Interceptor{
 	static int MAPPED_STATEMENT_INDEX = 0;
 	static int PARAMETER_INDEX = 1;
-	static int OFFSET_INDEX = 2;
-	static int LIMIT_INDEX = 3;
-	static int RESULT_HANDLER_INDEX = 4;
+	static int ROWBOUNDS_INDEX = 2;
+	static int RESULT_HANDLER_INDEX = 3;
 	
 	Dialect dialect;
 	
@@ -60,20 +61,22 @@ public class OffsetLimitInterceptor implements Interceptor{
 		//queryArgs = query(MappedStatement ms, Object parameter, int offset, int limit, ResultHandler resultHandler)
 		MappedStatement ms = (MappedStatement)queryArgs[MAPPED_STATEMENT_INDEX];
 		Object parameter = queryArgs[PARAMETER_INDEX];
-		final int offset = (Integer)queryArgs[OFFSET_INDEX];
-		final int limit = (Integer)queryArgs[LIMIT_INDEX];
+		final RowBounds rowBounds = (RowBounds)queryArgs[ROWBOUNDS_INDEX];
+		int offset = rowBounds.getOffset();
+		int limit = rowBounds.getLimit();
 		
-		if(dialect.supportsLimit() && (offset != Executor.NO_ROW_OFFSET || limit != Executor.NO_ROW_LIMIT)) {
+		if(dialect.supportsLimit() && (offset != RowBounds.NO_ROW_OFFSET || limit != RowBounds.NO_ROW_LIMIT)) {
 			BoundSql boundSql = ms.getBoundSql(parameter);
 			String sql = boundSql.getSql().trim();
 			if (dialect.supportsLimitOffset()) {
 				sql = dialect.getLimitString(sql, offset, limit);
-				queryArgs[OFFSET_INDEX] = Executor.NO_ROW_OFFSET;
+				limit = RowBounds.NO_ROW_LIMIT;
 			} else {
 				sql = dialect.getLimitString(sql, 0, limit);
 			}
-			queryArgs[LIMIT_INDEX] = Executor.NO_ROW_LIMIT;
+			offset = RowBounds.NO_ROW_OFFSET;
 			
+			queryArgs[ROWBOUNDS_INDEX] = new RowBounds(offset,limit);
 			BoundSql newBoundSql = new BoundSql(sql, boundSql.getParameterMappings(), boundSql.getParameterObject());
 			MappedStatement newMs = copyFromMappedStatement(ms, new BoundSqlSqlSource(newBoundSql));
 			queryArgs[MAPPED_STATEMENT_INDEX] = newMs;
