16a6a9fca0c4dd0175738b6c86c1f97196c5c067
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-10-15 12:47:06 +0000

    正式使用

---
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/test/hsql/HsqlMemDataSourceFactoryBean.java
@@ -0,0 +1,52 @@
+package cn.org.rapid_framework.test.hsql;
+
+import java.nio.charset.Charset;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.core.io.Resource;
+
+/**
+ * FactoryBean用于创建一个hsql内存数据库的DataSource并同时运行初始化的数据库脚本
+ * 
+ * @author badqiu
+ *
+ */
+
+public class HsqlMemDataSourceFactoryBean implements FactoryBean{
+	private Resource initScriptsLocation;
+	private String encoding = Charset.defaultCharset().name();
+	
+	public HsqlMemDataSourceFactoryBean(){}
+	
+	public HsqlMemDataSourceFactoryBean(Resource initScriptsLocation,String encoding) {
+		this.initScriptsLocation = initScriptsLocation;
+		this.encoding = encoding;
+	}
+
+	public void setInitScriptsLocation(Resource initScriptsLocation) {
+		this.initScriptsLocation = initScriptsLocation;
+	}
+	
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public Object getObject() throws Exception {
+		if(initScriptsLocation == null) {
+			return HsqlDataSourceUtils.getDataSource();
+		}else {
+			return HsqlDataSourceUtils.getDataSource(initScriptsLocation,encoding);
+		}
+	}
+
+	public Class getObjectType() {
+		return DataSource.class;
+	}
+
+	public boolean isSingleton() {
+		return true;
+	}
+
+}
