11ef1c99220284327abba173d7764c2fd0d85207
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-12-08 16:41:36 +0000

    重构

---
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/freemarker/template/Utils.java
@@ -0,0 +1,26 @@
+package cn.org.rapid_framework.freemarker.template;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import freemarker.core.Environment;
+import freemarker.template.TemplateException;
+
+class Utils {
+	
+	public static String BLOCK = "__block__";
+	
+	public static String getBlockName(String name) {
+		String realName = BLOCK + name;
+		return realName;
+	}
+	
+	public static String getRequiredParam(Map params,String key,Environment env) throws TemplateException {
+		Object value = params.get(key);
+		if(value == null || StringUtils.isEmpty(value.toString())) {
+			throw new TemplateException("not found required param:"+key,env);
+		}
+		return value.toString();
+	}
+}
