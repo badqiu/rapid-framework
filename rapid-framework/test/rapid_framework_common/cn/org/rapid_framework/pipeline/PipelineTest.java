30089bfb50aba3bc70a7afd1765e721f5aec9e0e
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2009-12-12 00:24:28 +0000

    分开freemarker,velocity pipeline

--- rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/pipeline/PipelineTest.java
+++ rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/pipeline/PipelineTest.java
@@ -20,47 +20,5 @@ import freemarker.template.Configuration;
 
 public class PipelineTest {
 	
-	VelocityEngine engine = new VelocityEngine();
-	Pipeline p = new Pipeline();
-	
-	@Before
-	public void setUp() throws FileNotFoundException, Exception {
-		File dir = ResourceUtils.getFile("classpath:fortest_velocity/pipeline");
-
-		Properties props = new Properties();
-		props.setProperty("userdirective","cn.org.rapid_framework.velocity.directive.BlockDirective,cn.org.rapid_framework.velocity.directive.OverrideDirective,cn.org.rapid_framework.velocity.directive.ExtendsDirective");
-		props.put(Velocity.FILE_RESOURCE_LOADER_PATH, dir.getAbsolutePath());
-		engine.init(props);
-	}
-	
-	@Test
-	public void testVelocity()  throws Exception  {
-		
-		StringWriter sw = new StringWriter();
-//		p.pipeline(engine,new String[] {"first.vm","second.vm","three.vm"}, new HashMap(), sw);
-		Pipeline.pipeline(engine,"first.vm|second.vm | three.vm", new HashMap(), sw);
-		System.out.println(sw.toString());
-		String expected = "<html><head>second_override_content</head><body>first_override_content<three><second>first</second></three></body></html>";
-		Assert.assertEquals(expected,sw.toString().replaceAll("\\s+", ""));
-	}
-	
-	Configuration conf = new Configuration();
-	@Before
-	public void setUpForFreemarker() throws FileNotFoundException, Exception {
-		FreemarkerTemplateProcessor.exposeRapidMacros(conf);
-		conf.setTemplateLoader(new FileTemplateLoader(ResourceUtils.getFile("classpath:fortest_freemarker/pipeline")));
-	}
-	@Test
-	public void testFreemarker()  throws Exception  {
-		
-		StringWriter out = new StringWriter();
-		HashMap model = new HashMap();
-		model.put("name", "badqiu");
-		Pipeline.pipeline(conf,"first.flt|second.flt | three.flt", model, out);
-		
-		System.out.println(out.toString());
-		String expected = "<html><head>override_by_first</head><body>override_by_second</body><three><second>first:badqiu</second></three></html>";
-		Assert.assertEquals(expected,out.toString().replaceAll("\\s+", ""));
-	}
 
 }
