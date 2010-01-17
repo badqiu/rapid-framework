2d07d3c37af431043d4b165fe5aa90e8dfe45e34
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-01-17 13:06:32 +0000

    spring jdbc orm 入库

---
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/jdbc/orm/SpringNamedSqlGeneratorTest.java
@@ -0,0 +1,78 @@
+package cn.org.rapid_framework.jdbc.orm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cn.org.rapid_framework.jdbc.orm.metadata.Column;
+import cn.org.rapid_framework.jdbc.orm.metadata.Table;
+
+
+public class SpringNamedSqlGeneratorTest {
+
+	Table table = new Table("user",new Column("user_id","userId",true),new Column("user_name","userName"),new Column("pwd","pwd"));
+	SqlGenerator t = new SpringNamedSqlGenerator(table);
+	
+	Table multiKeyTable = new Table("user",new Column("user_id","userId",true),new Column("group_id","groupId",true),new Column("user_name","userName"),new Column("pwd","pwd"));
+	SqlGenerator multiKeySqlGenerator = new SpringNamedSqlGenerator(multiKeyTable);
+	@Test
+	public void insertSql() {
+		System.out.println(t.getInsertSql());
+		assertEquals("INSERT user (user_id,user_name,pwd ) VALUES ( :userId,:userName,:pwd ) ", t.getInsertSql());
+	}
+	
+	@Test
+	public void updateSql() {
+		System.out.println(t.getUpdateSql());
+		assertEquals("UPDATE user SET (user_id = :userId,user_name = :userName,pwd = :pwd ) WHERE user_id = :userId", t.getUpdateSql());
+	}
+	
+	@Test
+	public void deleteSql() {
+		System.out.println(t.getDeleteByPrimaryKeysSql());
+		assertEquals("DELETE FROM user WHERE user_id = :userId", t.getDeleteByPrimaryKeysSql());
+		
+		assertEquals("DELETE FROM user WHERE user_id = :userId AND group_id = :groupId", multiKeySqlGenerator.getDeleteByPrimaryKeysSql());
+	}
+	
+	@Test
+	public void getDeleteBySinglePkSql() {
+		System.out.println(t.getDeleteBySinglePkSql());
+		assertEquals("DELETE FROM user WHERE user_id = :id", t.getDeleteBySinglePkSql());
+		
+		try {
+			multiKeySqlGenerator.getDeleteBySinglePkSql();
+			fail();
+		}catch(IllegalStateException espected) {
+			assertNotNull(espected);
+		}
+	}
+	
+	@Test
+	public void getSelectByPrimaryKeysSql() {
+		System.out.println(t.getSelectByPrimaryKeysSql());
+		assertEquals("SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :userId", t.getSelectByPrimaryKeysSql());
+		
+		assertEquals("SELECT user_id userId,group_id groupId,user_name userName,pwd pwd FROM user WHERE user_id = :userId AND group_id = :groupId", multiKeySqlGenerator.getSelectByPrimaryKeysSql());
+	}
+	
+	@Test
+	public void getSelectBySinglePkSql() {
+		System.out.println(t.getSelectBySinglePkSql());
+		assertEquals("SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :id", t.getSelectBySinglePkSql());
+		
+		try {
+			multiKeySqlGenerator.getSelectBySinglePkSql();
+			fail();
+		}catch(IllegalStateException espected) {
+			assertNotNull(espected);
+		}
+	}
+	
+	@Test
+	public void getColumnsSql() {
+		System.out.println(t.getColumnsSql());
+		assertEquals("user_id userId,user_name userName,pwd pwd", t.getColumnsSql());
+	}
+	
+}
---
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/jdbc/orm/SqlGenerator.java
@@ -0,0 +1,50 @@
+package cn.org.rapid_framework.jdbc.orm;
+
+/**
+ * 根据table映射生成增删改查的sql.
+ * 
+ * 
+ * @author badqiu
+ *
+ */
+public interface SqlGenerator {
+	/**
+	 * @return INSERT user (user_id,user_name,pwd ) VALUES ( :userId,:userName,:pwd ) 
+	 */
+	public String getInsertSql();
+	
+	/**
+	 * @return UPDATE user SET (user_id = :userId,user_name = :userName,pwd = :pwd ) WHERE user_id = :userId
+	 */
+	public String getUpdateSql();
+	
+	/**
+	 * 用于联合主键
+	 * @return DELETE FROM user WHERE user_id = :userId AND group_id = :groupId
+	 */
+	public String getDeleteByPrimaryKeysSql();
+	
+	/**
+	 * 用于单主键的表,使用固定的":id"来作为命名参数
+	 * @return DELETE FROM user WHERE user_id = :id 
+	 */
+	public String getDeleteBySinglePkSql();
+	
+	/**
+	 * 用于联合主键
+	 * @return SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :userId AND group_id = :groupId
+	 */
+	public String getSelectByPrimaryKeysSql();
+	
+	/**
+	 * 用于单主键的表,使用固定的":id"来作为命名参数
+	 * @return SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :id
+	 */
+	public String getSelectBySinglePkSql();
+	
+	/**
+	 * 得到列的映射
+	 * @return user_id userId,user_name userName,pwd pwd
+	 */
+	public String getColumnsSql();
+}
