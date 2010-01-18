5f61fdc13f08fd7ef8d0a21722e11719d14d3307
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-01-18 15:10:42 +0000

    增加column的annotation配置

--- rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/metadata/TableTest.java
+++ rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/metadata/TableTest.java
@@ -24,6 +24,22 @@ public class TableTest {
 		assertColumn(c2,"user_name","userName",false);
 		System.out.println(t);
 	}
+	
+	@Test
+	public void fromClassWithAnnotation() throws IntrospectionException {
+		Table t = Table.fromClass(AnnotationTestBean.class);
+
+		assertEquals("ann_test_bean",t.getTableName());
+		assertEquals(3,t.getColumns().size());
+
+		Column c0 = t.getColumns().get(0);
+		assertColumn(c0,"ann_age","age",false);
+		Column c1 = t.getColumns().get(1);
+		assertColumn(c1,"password","password",false);
+		Column c2 = t.getColumns().get(2);
+		assertColumn(c2,"ann_id","userName",true);
+		System.out.println(t);
+	}
 
 	private void assertColumn(Column c, String sqlName, String propertyName,
 			boolean isPrimaryKey) {
