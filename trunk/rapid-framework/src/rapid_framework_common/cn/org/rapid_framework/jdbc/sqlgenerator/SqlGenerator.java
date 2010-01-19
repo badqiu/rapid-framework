3f0cc728193300294ce8e17daad2d43fcd48256a
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-01-19 15:31:59 +0000

    1.增加inserable updatable的支持 2.使用?号替换:id 3.修改注释

--- trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/SqlGenerator.java
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/SqlGenerator.java
@@ -3,7 +3,7 @@ package cn.org.rapid_framework.jdbc.sqlgenerator;
 import cn.org.rapid_framework.jdbc.sqlgenerator.metadata.Table;
 
 /**
- * 根据table映射生成增删改查的sql工具类.
+ * 根据Table对象生成增删改查sql工具类.
  *
  * @see Table
  * @author badqiu
@@ -16,26 +16,39 @@ public interface SqlGenerator {
 	public String getInsertSql();
 
 	/**
-	 * 单主键的表,使用固定的":id"来作为命名参数,多主键则使用各自的column propertyName作为命名参数
-	 * @return UPDATE user SET (user_id = :userId,user_name = :userName,pwd = :pwd ) WHERE user_id = :id
+	 * 单主键的表,使用固定的"?"来作为参数,联合主键则使用各自的column propertyName作为命名参数
+	 * 
+	 * <pre>
+	 * 单主键: UPDATE user SET (user_id = :userId,user_name = :userName ) WHERE user_id = ?
+	 * 联合主键: UPDATE user SET (user_id = :userId,user_name = :userName ) WHERE user_id = :user_id AND group_id = :group_id
+	 * </pre>
 	 */
 	public String getUpdateByPkSql();
 
 	/**
-	 * 用于单主键的表,使用固定的":id"来作为命名参数,多主键则使用各自的column propertyName作为命名参数
-	 * @return DELETE FROM user WHERE user_id = :id
+	 * 
+	 * 单主键的表,使用固定的"?"来作为参数,联合主键则使用各自的column propertyName作为命名参数
+	 * 
+	 * <pre>
+	 * 单主键: DELETE FROM user WHERE user_id = ?
+	 * 联合主键: DELETE FROM user WHERE user_id = :user_id AND group_id = :group_id
+	 * </pre>
 	 */
 	public String getDeleteByPkSql();
 
 	/**
-	 * 单主键的表,使用固定的":id"来作为命名参数,多主键则使用各自的column propertyName作为命名参数
-	 * @return SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :id
+	 * 单主键的表,使用固定的"?"来作为参数,联合主键则使用各自的column propertyName作为命名参数
+	 * 
+	 * <pre>
+	 * 单主键: SELECT user_id userId,user_name userName FROM user WHERE user_id = ?
+	 * 联合主键: SELECT user_id userId,user_name userName FROM user WHERE user_id = :userId AND group_id = :group_id
+	 * </pre>
 	 */
 	public String getSelectByPkSql();
 
 	/**
-	 * 得到列的映射
-	 * @return user_id userId,user_name userName,pwd pwd
+	 * 得到列的sql段用于其它sql的拼接查询
+	 * @return user_id userId,user_name userName
 	 */
 	public String getColumnsSql();
 }
--- trunk/rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/SqlGeneratorTest.java
+++ trunk/rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/jdbc/sqlgenerator/SqlGeneratorTest.java
@@ -20,22 +20,35 @@ public class SqlGeneratorTest {
 	}
 	@Test
 	public void getUpdateByPkSql(){
-		assertEquals("UPDATE user SET (user_id = :userId,user_name = :userName,pwd = :pwd ) WHERE user_id = :id",singleGenerator.getUpdateByPkSql());
+		assertEquals("UPDATE user SET (user_id = :userId,user_name = :userName,pwd = :pwd ) WHERE user_id = ?",singleGenerator.getUpdateByPkSql());
 		assertEquals("UPDATE user SET (user_id = :userId,group_id = :groupId,user_name = :userName,pwd = :pwd ) WHERE user_id = :userId AND group_id = :groupId",multiGenerator.getUpdateByPkSql());
 	}
 	@Test
 	public void getDeleteByPkSql(){
-		assertEquals("DELETE FROM user WHERE user_id = :id",singleGenerator.getDeleteByPkSql());
+		assertEquals("DELETE FROM user WHERE user_id = ?",singleGenerator.getDeleteByPkSql());
 		assertEquals("DELETE FROM user WHERE user_id = :userId AND group_id = :groupId",multiGenerator.getDeleteByPkSql());
 	}
 	@Test
 	public void getSelectByPkSql(){
-		assertEquals("SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = :id",singleGenerator.getSelectByPkSql());
+		assertEquals("SELECT user_id userId,user_name userName,pwd pwd FROM user WHERE user_id = ?",singleGenerator.getSelectByPkSql());
 		assertEquals("SELECT user_id userId,group_id groupId,user_name userName,pwd pwd FROM user WHERE user_id = :userId AND group_id = :groupId",multiGenerator.getSelectByPkSql());
 	}
 	@Test
 	public void getColumnsSql(){
 		assertEquals("user_id userId,user_name userName,pwd pwd",singleGenerator.getColumnsSql());
 		assertEquals("user_id userId,group_id groupId,user_name userName,pwd pwd",multiGenerator.getColumnsSql());
+	}
+	
+	Table t3 = new Table("user",new Column("user_id","userId",true),new Column("user_name","userName",false,true,false,false),new Column("pwd","pwd",false,false,true,false));
+	SqlGenerator t3g = new SpringNamedSqlGenerator(t3);
+	Table t4 = new Table("user",new Column("user_id","userId",true),new Column("group_id","groupId",true),new Column("user_name","userName",false,false,false,false));
+	SqlGenerator t4g = new SpringNamedSqlGenerator(t4);
+	@Test
+	public void test_insertable_and_updatable() {
+		assertEquals("INSERT user (user_id,pwd ) VALUES ( :userId,:pwd ) ",t3g.getInsertSql());
+		assertEquals("UPDATE user SET (user_id = :userId,user_name = :userName ) WHERE user_id = :id",t3g.getUpdateByPkSql());
+		
+		assertEquals("INSERT user (user_id,group_id ) VALUES ( :userId,:groupId ) ",t4g.getInsertSql());
+		assertEquals("UPDATE user SET (user_id = :userId,group_id = :groupId ) WHERE user_id = :userId AND group_id = :groupId",t4g.getUpdateByPkSql());
 	}
 }
