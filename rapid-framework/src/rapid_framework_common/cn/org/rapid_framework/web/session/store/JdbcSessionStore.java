8dc17acdfd3e83de34cbf08e6c35f7ade7e469b3
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-01-26 15:48:22 +0000

    修正jdbc session store

---
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/web/session/store/JdbcSessionStore.java
@@ -0,0 +1,79 @@
+package cn.org.rapid_framework.web.session.store;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+/**
+ * <pre>
+ *	CREATE TABLE http_session_store (
+ *	  session_id char(40) PRIMARY KEY,
+ *	  session_data text,
+ *	  expire_date bigint
+ *	);
+ * </pre>
+ *
+ * @author badqiu
+ *
+ */
+public class JdbcSessionStore extends SessionStore{
+	DataSource dataSource;
+
+	static String DELETE = "delete from http_session_store where session_id = ?";
+	static String INSERT = "insert into http_session_store(session_id,expire_date,session_data) values (?,?,?)";
+	static String GET = "select session_data from http_session_store where session_id = ? and expire_date >= ?";
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public void deleteSession(HttpServletResponse response,String sessionId) {
+		getSimpleJdbcTemplate().update(DELETE, sessionId);
+	}
+
+	public Map getSession(HttpServletRequest request, String sessionId,int timeoutSeconds) {
+		List<Map> results = getSimpleJdbcTemplate().query(GET,new ParameterizedRowMapper<Map>(){
+			public Map mapRow(ResultSet rs, int row) throws SQLException {
+				String sessionData = rs.getString(1);
+				return decode(sessionData);
+			}
+		},sessionId,System.currentTimeMillis());
+		
+		return results.size() > 0 ? results.get(0) : new HashMap();
+	}
+
+	public void saveSession(HttpServletResponse response, String sessionId,Map sessionData,int timeoutSeconds) {
+		deleteSession(response, sessionId);
+		String data = encode(sessionData);
+		Timestamp expire_date = computeExpireDate(timeoutSeconds);
+		getSimpleJdbcTemplate().update(INSERT, sessionId,expire_date.getTime(),data);
+	}
+
+	private Timestamp computeExpireDate(int timeoutSeconds) {
+//		Timestamp now = new Timestamp(System.currentTimeMillis());
+//		CalendarUtils.add(Calendar.SECOND, now, timeoutSeconds);
+//		return now;
+		return new Timestamp(System.currentTimeMillis()+ (timeoutSeconds * 1000));
+	}
+
+	private SimpleJdbcTemplate getSimpleJdbcTemplate() {
+		return new SimpleJdbcTemplate(dataSource);
+	}
+
+	public static Map decode(String sessionData) {
+		return SessionDataUtils.decode(sessionData);
+	}
+
+	public static String encode(Map sessionData) {
+		return SessionDataUtils.encode(sessionData);
+	}
+}
