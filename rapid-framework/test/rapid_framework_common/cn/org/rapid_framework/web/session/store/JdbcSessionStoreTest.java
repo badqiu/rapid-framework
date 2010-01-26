ef989e08d9725321592c6e0fa81ca1da46f6c1ba
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-01-26 15:53:53 +0000

    增加test_timeout

--- rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/web/session/store/JdbcSessionStoreTest.java
+++ rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/web/session/store/JdbcSessionStoreTest.java
@@ -3,6 +3,7 @@ package cn.org.rapid_framework.web.session.store;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -15,9 +16,10 @@ public class JdbcSessionStoreTest {
 	JdbcSessionStore store = new JdbcSessionStore();
 	MockHttpServletRequest request = new MockHttpServletRequest();
 	MockHttpServletResponse response = new MockHttpServletResponse();
-	@Test
-	public void test() {
-		Map sessionData = new HashMap();
+	Map sessionData = new HashMap();
+	
+	@Before
+	public void setUp() {
 		sessionData.put("empty", "");
 		sessionData.put("blank", " ");
 		sessionData.put("null", null);
@@ -25,23 +27,42 @@ public class JdbcSessionStoreTest {
 		
 		String sql = "	CREATE TABLE http_session_store (session_id char(40) PRIMARY KEY,session_data varchar(4000),expire_date bigint ) ";
 		store.setDataSource(HSQLMemDataSourceUtils.getDataSource(sql));
+	}
+	
+	@Test
+	public void test_get_and_delete() {
 
 		Map map = store.getSession(request, "123",100);
 		assertEquals(map.size(), 0);
 		
 		store.saveSession(response, "123", sessionData,100);
 		
-		
+		//test get
 		map = store.getSession(request, "123",5);
 		assertEquals(map.size(), 4);
 		assertMapEquals(map);
 		
+		//test delete
 		store.deleteSession(response, "123");
-		
 		map = store.getSession(request, "123",100);
 		assertEquals(map.size(), 0);
 		
 	}
+	
+	@Test
+	public void test_timeout() throws InterruptedException {
+		
+		store.saveSession(response, "123", sessionData,3);
+		
+		Thread.sleep(5 * 1000);
+		//test get
+		Map map = store.getSession(request, "123",0);
+		assertEquals(0,map.size());
+		
+		
+	}
+	
+	
 	private void assertMapEquals(Map map) {
 		
 	}
