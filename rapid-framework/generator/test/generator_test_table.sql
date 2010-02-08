7c56e233cd36e3c3ae360eaa477bcc1741730151
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-02-08 04:16:32 +0000

    增加generator 单元测试

--- rapid-framework/generator/test/generator_test_table.sql
+++ rapid-framework/generator/test/generator_test_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE user_info (
+CREATE TABLE USER_INFO (
   user_id bigint NOT NULL PRIMARY KEY,
   username varchar(50) DEFAULT NULL,
   password varchar(50) DEFAULT NULL,
   birth_date date DEFAULT NULL,
   sex TINYINT DEFAULT NULL,
   age INTEGER DEFAULT NULL
-);
+)
 
