456b1dbc5c3ccf75d432ff31f9f168c23085e045
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-03-24 07:13:40 +0000

    add isEmtpy(Object) isNotEmpty(Object)

--- trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/ObjectUtils.java
+++ trunk/rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/util/ObjectUtils.java
@@ -1,4 +1,8 @@
 package cn.org.rapid_framework.util;
+
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * @author badqiu
  */
@@ -13,6 +17,40 @@ public class ObjectUtils {
 				return true;
 		}
 		return false;
+	}
+	
+	public static boolean isEmpty(Object c) {
+		if(c == null) return true;
+		boolean isSequence = c instanceof Map || c instanceof Collection || c instanceof String || c.getClass().isArray();
+		if(!isSequence) {
+			throw new IllegalArgumentException("illegal argument type,must be : Map,Collection,Array,String");
+		}
+		
+		if(c instanceof Map) {
+			if(((Map)c).isEmpty()){
+				return true;
+			}
+		}
+		if(c instanceof Collection) {
+			if(((Collection)c).isEmpty()){
+				return true;
+			}
+		}
+		if(c.getClass().isArray()) {
+			if(((Object[])c).length == 0){
+				return true;
+			}
+		}
+		if(c instanceof String) {
+			if(((String)c).length() == 0){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isNotEmpty(Object c) {
+		return !isEmpty(c);
 	}
 	
 }
