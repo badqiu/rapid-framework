75a703eed23a18424f4b395285aa877a9577c59b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-03-25 16:04:38 +0000

    delete commons

--- rapid-framework/generator/test/cn/org/rapid_framework/generator/provider/db/model/ColumnTest.java
+++ rapid-framework/generator/test/cn/org/rapid_framework/generator/provider/db/model/ColumnTest.java
@@ -3,8 +3,7 @@ package cn.org.rapid_framework.generator.provider.db.model;
 import java.lang.reflect.InvocationTargetException;
 
 import junit.framework.TestCase;
-
-import org.apache.commons.beanutils.BeanUtils;
+import cn.org.rapid_framework.generator.util.BeanHelper;
 
 
 
@@ -12,6 +11,6 @@ public class ColumnTest extends TestCase{
 	
 	public void testColumn() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		Column c = new Column(new Table(),1,"int","user_name",1,2,true,true,true,true,"","remarks");
-		System.out.println(BeanUtils.describe(c));
+		System.out.println(BeanHelper.describe(c));
 	}
 }
