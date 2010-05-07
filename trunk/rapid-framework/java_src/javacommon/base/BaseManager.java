dd49fad426c5fed6143c0dfc457a30f8bf17c363
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-05-07 03:00:00 +0000

    增加 throws DataAccessException 及注释

--- trunk/rapid-framework/java_src/javacommon/base/BaseManager.java
+++ trunk/rapid-framework/java_src/javacommon/base/BaseManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 /**
  * @author badqiu
@@ -17,33 +18,36 @@ public abstract class BaseManager <E,PK extends Serializable>{
 	protected abstract EntityDao getEntityDao();
 
 	@Transactional(readOnly=true)
-	public E getById(PK id) {
+	public E getById(PK id) throws DataAccessException{
 		return (E)getEntityDao().getById(id);
 	}
 	
 	@Transactional(readOnly=true)
-	public List<E> findAll() {
+	public List<E> findAll() throws DataAccessException{
 		return getEntityDao().findAll();
 	}
 	
-	public void saveOrUpdate(E entity) {
+	/** 根据id检查是否插入或是更新数据 */
+	public void saveOrUpdate(E entity) throws DataAccessException{
 		getEntityDao().saveOrUpdate(entity);
 	}
 	
-	public void save(E entity) {
+	/** 插入数据 */
+	public void save(E entity) throws DataAccessException{
 		getEntityDao().save(entity);
 	}
 	
-	public void removeById(PK id) {
+	public void removeById(PK id) throws DataAccessException{
 		getEntityDao().deleteById(id);
 	}
 	
-	public void update(E entity) {
+	public void update(E entity) throws DataAccessException{
 		getEntityDao().update(entity);
 	}
 	
 	@Transactional(readOnly=true)
-	public boolean isUnique(E entity, String uniquePropertyNames) {
+	public boolean isUnique(E entity, String uniquePropertyNames) throws DataAccessException {
 		return getEntityDao().isUnique(entity, uniquePropertyNames);
 	}
+	
 }
--- trunk/rapid-framework/java_src/javacommon/base/EntityDao.java
+++ trunk/rapid-framework/java_src/javacommon/base/EntityDao.java
@@ -2,25 +2,27 @@ package javacommon.base;
 
 import java.io.Serializable;
 import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 /**
  * @author badqiu
  */
 public interface EntityDao <E,PK extends Serializable>{
 
-	public Object getById(PK id);
+	public Object getById(PK id) throws DataAccessException;
 	
-	public void deleteById(PK id);
+	public void deleteById(PK id) throws DataAccessException;
 	
-	public void save(E entity);
+	public void save(E entity) throws DataAccessException;
 	
-	public void update(E entity);
+	public void update(E entity) throws DataAccessException;
 
-	public void saveOrUpdate(E entity);
+	public void saveOrUpdate(E entity) throws DataAccessException;
 
-	public boolean isUnique(E entity, String uniquePropertyNames);
+	public boolean isUnique(E entity, String uniquePropertyNames) throws DataAccessException;
 	
-	public void flush();
+	public void flush() throws DataAccessException;
 	
-	public List<E> findAll();
+	public List<E> findAll() throws DataAccessException;
 	
 }
