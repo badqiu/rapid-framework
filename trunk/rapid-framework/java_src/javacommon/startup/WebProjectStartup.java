fe5926a9a8984b4adac6bb36f7ab7fa79929e581
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-05-09 05:48:33 +0000

    增加 WebProjectStartup web.xml注册

---
+++ trunk/rapid-framework/java_src/javacommon/startup/WebProjectStartup.java
@@ -0,0 +1,26 @@
+package javacommon.startup;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * web项目系统启动时会执行一些初始化操作,该类已经在web.xml中注册
+ * 
+ * <br />
+ * 	<listener>
+ *		<listener-class>javacommon.startup.WebProjectStartup</listener-class>
+ *	</listener>
+ * @author badqiu
+ */
+public class WebProjectStartup implements ServletContextListener{
+
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext context = event.getServletContext();
+	}
+	
+	public void contextDestroyed(ServletContextEvent event) {
+		ServletContext context = event.getServletContext();
+	}
+
+}
