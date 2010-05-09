838a7988978796542042a4323ca882b576990ab1
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-05-09 04:31:38 +0000

    add exception

---
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package cn.org.rapid_framework.exception;
+
+/**
+ * 错误码,ERROR_CODE的设计目标是从google根据error code查到到原因.
+ * 如 ORA-95539
+ * @author badqiu
+ *
+ */
+public interface ErrorCode extends java.io.Serializable{
+
+	public String getErrorCode();
+
+	public String getErrorDetails();
+
+}
---
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/exception/ErrorCodeException.java
@@ -0,0 +1,29 @@
+package cn.org.rapid_framework.exception;
+
+public class ErrorCodeException extends RuntimeException{
+	//TODO 考虑是否需要suberror, 如parernt error=1000,child error=500, full_error=1000.500
+	private ErrorCode errorCode;
+	
+	public ErrorCodeException(ErrorCode errorCode) {
+		super();
+		this.errorCode = errorCode;
+	}
+	
+	public ErrorCodeException(ErrorCode errorCode,Throwable cause) {
+		super(cause);
+		this.errorCode = errorCode;
+	}
+	
+	public ErrorCode getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(ErrorCode errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String toString() {
+		return String.format("errorCode:[%s],errorDetails:%s",errorCode.getErrorCode(),errorCode.getErrorDetails());
+	}
+	
+}
