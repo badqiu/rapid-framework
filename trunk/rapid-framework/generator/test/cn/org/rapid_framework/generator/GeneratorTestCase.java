15f8a615751feaae05ca42f99661123ea64f177b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-06-03 17:12:22 +0000

    generator

--- trunk/rapid-framework/generator/test/cn/org/rapid_framework/generator/GeneratorTestCase.java
+++ trunk/rapid-framework/generator/test/cn/org/rapid_framework/generator/GeneratorTestCase.java
@@ -60,13 +60,8 @@ public class GeneratorTestCase extends TestCase{
 	
 	public void generateByTable(Table table) throws Exception {
 		GeneratorModel m = GeneratorModel.newFromTable(table);
-		g.setIgnoreTemplateGenerateException(false);
-		List<Exception> exceptions = g.generateBy(m.templateModel, m.filePathModel);
-		for(Exception e : exceptions) {
-		    e.printStackTrace();
-		    throw e;
-		}
-		
+//		g.setIgnoreTemplateGenerateException(false);
+		g.generateBy(m.templateModel, m.filePathModel);
 	}
 	
 	public void generateByTable(Generator g,Table table) throws Exception {
