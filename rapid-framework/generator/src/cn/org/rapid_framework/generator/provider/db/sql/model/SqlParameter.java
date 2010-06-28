fd770b9130911ce76647b790e6938cf66a0421d8
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-06-28 11:59:45 +0000

    将SqlParameter继承之Column

--- rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/sql/model/SqlParameter.java
+++ rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/sql/model/SqlParameter.java
@@ -3,43 +3,54 @@ package cn.org.rapid_framework.generator.provider.db.sql.model;
 import java.sql.ParameterMetaData;
 import java.sql.SQLException;
 
-public class SqlParameter {
-    	String parameterClassName;
-    	int parameterMode;
-    	int parameterType;
-    	String parameterTypeName;
-    	int precision;
-    	int scale;
+import cn.org.rapid_framework.generator.provider.db.table.model.Column;
+
+public class SqlParameter extends Column {
+//    	String parameterClassName;
+//    	int parameterMode;
+//    	int parameterType;
+//    	String parameterTypeName;
+//    	int precision;
+//    	int scale;
     	String paramName;
     	boolean isListParam = false;
+    	boolean cannotGuessParamType = false; //是否是不能从数据库metadata猜测出数据类型的column
+    	
     	public SqlParameter() {}
-    	
+
+        public SqlParameter(Column param) {
+            super(param);
+        }
+        
     	public SqlParameter(SqlParameter param) {
+    	    super(param);
     		this.isListParam = param.isListParam;
-    		this.parameterClassName = param.parameterClassName;
-    		this.parameterMode = param.parameterMode;
-    		this.parameterType = param.parameterType;
-    		this.parameterTypeName = param.parameterTypeName;
     		this.paramName = param.paramName;
-    		this.precision = param.precision;
-    		this.scale = param.scale;
+    		
+//    		this.parameterClassName = param.parameterClassName;
+//    		this.parameterMode = param.parameterMode;
+//    		this.parameterType = param.parameterType;
+//    		this.parameterTypeName = param.parameterTypeName;
+//    		this.precision = param.precision;
+//    		this.scale = param.scale;
     	}
     	
-    	public SqlParameter(ParameterMetaData m,int i) throws SQLException {
-    		this.parameterClassName = m.getParameterClassName(i);
-    		this.parameterMode = m.getParameterMode(i);
-    		this.parameterType = m.getParameterType(i);
-    		this.parameterTypeName = m.getParameterTypeName(i);
-    		this.precision = m.getPrecision(i);
-    		this.scale = m.getScale(i);
-    	}
-		public String getParameterClassName() {
-			return parameterClassName;
-		}
-		public void setParameterClassName(String parameterClassName) {
-			this.parameterClassName = parameterClassName;
-		}
+//    	public SqlParameter(ParameterMetaData m,int i) throws SQLException {
+//    		this.parameterClassName = m.getParameterClassName(i);
+//    		this.parameterMode = m.getParameterMode(i);
+//    		this.parameterType = m.getParameterType(i);
+//    		this.parameterTypeName = m.getParameterTypeName(i);
+//    		this.precision = m.getPrecision(i);
+//    		this.scale = m.getScale(i);
+//    	}
+//		public String getParameterClassName() {
+//			return parameterClassName;
+//		}
+//		public void setParameterClassName(String parameterClassName) {
+//			this.parameterClassName = parameterClassName;
+//		}
 		public String getPreferredParameterClassName() {
+		    String parameterClassName = getJavaType();
 			if(isListParam) {
 				if(parameterClassName.indexOf("[]") >= 0){
 					return parameterClassName;
@@ -55,36 +66,36 @@ public class SqlParameter {
 				return parameterClassName;
 			}
 		}
-		public int getParameterMode() {
-			return parameterMode;
-		}
-		public void setParameterMode(int parameterMode) {
-			this.parameterMode = parameterMode;
-		}
-		public int getParameterType() {
-			return parameterType;
-		}
-		public void setParameterType(int parameterType) {
-			this.parameterType = parameterType;
-		}
-		public String getParameterTypeName() {
-			return parameterTypeName;
-		}
-		public void setParameterTypeName(String parameterTypeName) {
-			this.parameterTypeName = parameterTypeName;
-		}
-		public int getPrecision() {
-			return precision;
-		}
-		public void setPrecision(int precision) {
-			this.precision = precision;
-		}
-		public int getScale() {
-			return scale;
-		}
-		public void setScale(int scale) {
-			this.scale = scale;
-		}
+//		public int getParameterMode() {
+//			return parameterMode;
+//		}
+//		public void setParameterMode(int parameterMode) {
+//			this.parameterMode = parameterMode;
+//		}
+//		public int getParameterType() {
+//			return parameterType;
+//		}
+//		public void setParameterType(int parameterType) {
+//			this.parameterType = parameterType;
+//		}
+//		public String getParameterTypeName() {
+//			return parameterTypeName;
+//		}
+//		public void setParameterTypeName(String parameterTypeName) {
+//			this.parameterTypeName = parameterTypeName;
+//		}
+//		public int getPrecision() {
+//			return precision;
+//		}
+//		public void setPrecision(int precision) {
+//			this.precision = precision;
+//		}
+//		public int getScale() {
+//			return scale;
+//		}
+//		public void setScale(int scale) {
+//			this.scale = scale;
+//		}
 		public String getParamName() {
 			return paramName;
 		}
@@ -112,6 +123,6 @@ public class SqlParameter {
 			return paramName.hashCode();
 		}
 		public String toString() {
-			return "paramName:"+paramName+" parameterClassName:"+parameterClassName;
+			return "paramName:"+paramName+" parameterClassName:"+getPreferredParameterClassName();
 		}
     }
