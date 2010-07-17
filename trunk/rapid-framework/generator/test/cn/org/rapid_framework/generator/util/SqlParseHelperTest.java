8f1b0e764af721dcfc999657bb903169d581404d
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-07-17 06:19:00 +0000

    增加表的别名解析

--- trunk/rapid-framework/generator/test/cn/org/rapid_framework/generator/util/SqlParseHelperTest.java
+++ trunk/rapid-framework/generator/test/cn/org/rapid_framework/generator/util/SqlParseHelperTest.java
@@ -4,28 +4,45 @@ import java.util.Set;
 
 import junit.framework.TestCase;
 import cn.org.rapid_framework.generator.util.sqlparse.SqlParseHelper;
+import cn.org.rapid_framework.generator.util.sqlparse.SqlParseHelper.SqlAlias;
 
 
 public class SqlParseHelperTest extends TestCase{
 	
 	public void test_getTableNamesByQuery_with_single_table() {
-		Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user");
+		Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user");
+		System.out.println(tableNames);
+		verifyTableNames(tableNames,"user");
+		
+		tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user t");
+		System.out.println(tableNames);
+		verifyTableNames(tableNames,"user");
+		
+		tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user as t");
 		System.out.println(tableNames);
 		verifyTableNames(tableNames,"user");
 	}
 	
 	public void test_getTableNamesByQuery_with_multi_table() {
-	    Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user,role");
+	    Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user,role");
 		System.out.println(tableNames);
 		verifyTableNames(tableNames,"user","role");
 		
 	    tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user,user_role,blog");
 	    System.out.println(tableNames);
 	    verifyTableNames(tableNames,"user","user_role","blog");
+	    
+	    tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user t1,user_role t2,blog t3");
+	    System.out.println(tableNames);
+	    verifyTableNames(tableNames,"user","user_role","blog");
+	    
+	    tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user as t1,user_role as t2,blog as t3");
+	    System.out.println(tableNames);
+	    verifyTableNames(tableNames,"user","user_role","blog");
 	}
 
     public void test_getTableNamesByQuery_with_multi_table2() {
-        Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u,role r where abc=123");
+        Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u,role r where abc=123");
         System.out.println(tableNames);
         verifyTableNames(tableNames,"user","role");
         
@@ -35,37 +52,38 @@ public class SqlParseHelperTest extends TestCase{
     }
 	   
     public void test_getTableNamesByQuery_with_join() {
-        Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u left join role r");
+        Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u left join role r on u.username=r.username");
         System.out.println(tableNames);
         verifyTableNames(tableNames,"user","role");
         
-        tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u inner join role r");
+        tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u inner join role r on u.username = r.username left join blog as b on u.username = b.username");
         System.out.println(tableNames);
-        verifyTableNames(tableNames,"user","role");
+        verifyTableNames(tableNames,"user","role","blog");
         
-        tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u left join role r");
+        tableNames = SqlParseHelper.getTableNamesByQuery("select * froM user u left join role r on u.username = b.username");
         System.out.println(tableNames);
         verifyTableNames(tableNames,"user","role");
     }
     
     public void test_verify_delete() {
-    	Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("delete from user_Info where username=:abc");
+    	Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("delete from user_Info where username=:abc");
     	 verifyTableNames(tableNames,"user_Info");
     }
 
     public void test_verify_update() {
-    	Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("update user_info set username = :username where password = :password and age=:age and sex=:sex");
+    	Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("update user_info set username = :username where password = :password and age=:age and sex=:sex");
     	verifyTableNames(tableNames,"user_info");
     }
 
     public void test_verify_insert() {
-    	Set<String> tableNames = SqlParseHelper.getTableNamesByQuery("insert into user_info values(:username,:password,:age,:sex,:userid,:blog)");
+    	Set<SqlAlias> tableNames = SqlParseHelper.getTableNamesByQuery("insert into user_info values(:username,:password,:age,:sex,:userid,:blog)");
     	verifyTableNames(tableNames,"user_info");
     }
     
-	private void verifyTableNames(Set<String> tableNames,String... expectedTableNames) {
+	private void verifyTableNames(Set<SqlAlias> tableNames,String... expectedTableNames) {
 		for(int i = 0; i < expectedTableNames.length; i++) {
-			assertTrue("actual tableNames:"+tableNames.toString(),tableNames.contains(expectedTableNames[i]));
+			boolean expected = tableNames.contains(new SqlAlias(expectedTableNames[i],null));
+			assertTrue("actual tableNames:"+tableNames.toString(),expected);
 		}
 	}
 	
