ea1b1d58a540351a3c817ef4cf0309ca34c3bd40
lipengtao@gmail.com <lipengtao@gmail.com@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-07-21 03:49:51 +0000

    refactory the test case up to standard.

--- rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/beanutils/BeanUtilsTest.java
+++ rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/beanutils/BeanUtilsTest.java
@@ -3,14 +3,17 @@ package cn.org.rapid_framework.beanutils;
 import java.lang.reflect.InvocationTargetException;
 
 import junit.framework.TestCase;
-
-import org.apache.commons.beanutils.PropertyUtils;
-
 import cn.org.rapid_framework.util.fortest.Role;
 
 public class BeanUtilsTest extends TestCase {
 	
-	Role role = new Role();
+	private Role role;
+	
+	@Override
+	protected void setUp(){
+		role = new Role();
+	}
+	
 	public void testSetBeanProperty() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		role.setRoleId(new Long(1));
 		BeanUtils.setProperty(role, "roleId", "2");
