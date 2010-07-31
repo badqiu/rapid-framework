9c754cfe8ac193dec49dfddd796db680a4837cc1
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-07-31 07:28:48 +0000

    add oracle tableconfig test

---
+++ trunk/rapid-framework/generator/src_ext/cn/org/rapid_framework/generator/ext/ibatis/model/OracleTableConfigTest.java
@@ -0,0 +1,106 @@
+package cn.org.rapid_framework.generator.ext.ibatis.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.org.rapid_framework.generator.GeneratorProperties;
+import cn.org.rapid_framework.generator.GeneratorTestCase;
+import cn.org.rapid_framework.generator.Generator.GeneratorModel;
+import cn.org.rapid_framework.generator.GeneratorFacade.GeneratorModelUtils;
+import cn.org.rapid_framework.generator.provider.db.sql.model.Sql;
+import cn.org.rapid_framework.generator.util.BeanHelper;
+import cn.org.rapid_framework.generator.util.FileHelper;
+
+public class OracleTableConfigTest extends GeneratorTestCase {
+    
+    public void setUp() throws Exception {
+//        super.setUp();
+    	System.setProperty("gg.isOverride", "true");
+		GeneratorProperties.setProperty("jdbc.url", "jdbc:oracle:thin:@localhost:1521:xe");
+		GeneratorProperties.setProperty("jdbc.driver", "oracle.jdbc.driver.OracleDriver");
+		GeneratorProperties.setProperty("jdbc.username", "sys as sysdba");
+		GeneratorProperties.setProperty("jdbc.password", "123456");
+		GeneratorProperties.setProperty("jdbc.schema", "SYS");
+		GeneratorProperties.setProperty("jdbc.catalog", "");
+		try {
+			runSqlScripts("generator/test/oracle_generator_test_table.sql");
+		}catch(Exception e) {
+			if(e.getMessage().contains("ORA-00955")) {
+			}else {
+				e.printStackTrace();
+			}
+		}
+		
+        g.setOutRootDir("./temp/generate_by_sql_config");
+        GeneratorProperties.setProperty("appName", "rapid");
+    }
+    
+    public void test_genereate_by_sql_config() throws Exception {
+        g.setTemplateRootDir(FileHelper.getFileByClassLoader("for_generate_by_sql_config"));
+        File file = FileHelper.getFileByClassLoader("cn/org/rapid_framework/generator/ext/ibatis/oracle_user_info.xml");
+        TableConfig t = TableConfig.parseFromXML(new FileInputStream(file));
+        GeneratorModel gm = newFromTable(t);
+        g.generateBy(gm.templateModel, gm.filePathModel);
+        
+    }
+
+    public void test_generate_by_sql() throws Exception {
+        g.setTemplateRootDir(FileHelper.getFileByClassLoader("for_generate_by_sql"));
+        File file = FileHelper.getFileByClassLoader("cn/org/rapid_framework/generator/ext/ibatis/oracle_user_info.xml");
+        TableConfig t = TableConfig.parseFromXML(new FileInputStream(file));
+        for(Sql sql : t.getSqls()) {
+            GeneratorModel gm = newFromSql(sql,t);
+            g.generateBy(gm.templateModel, gm.filePathModel);
+        }
+    }
+    
+	public void test_remove_table_prefix() {
+		GeneratorProperties.setProperty("tableRemovePrefixes", "t_,v_");
+		TableConfig sql = new TableConfig();
+		sql.setSqlname("t_user_info");
+		assertEquals("UserInfo",sql.getTableClassName());
+		sql.setSqlname("v_user");
+		assertEquals("User",sql.getTableClassName());
+		
+		sql.setSqlname("diy_user");
+		assertEquals("DiyUser",sql.getTableClassName());
+	}
+	
+    
+    private GeneratorModel newFromSql(Sql sql, TableConfig t) {
+        Map templateModel = new HashMap();
+        templateModel.putAll(GeneratorProperties.getProperties());
+        templateModel.put("sql", sql);
+        templateModel.put("tableConfig", t);
+        setShareVars(templateModel);
+        
+        Map filePathModel = new HashMap();
+        filePathModel.putAll(GeneratorProperties.getProperties());
+        filePathModel.putAll(BeanHelper.describe(t));
+        filePathModel.putAll(BeanHelper.describe(sql));
+        setShareVars(filePathModel);
+        GeneratorModel gm = new GeneratorModel(templateModel,filePathModel);
+        return gm;
+    }
+
+    private GeneratorModel newFromTable(TableConfig t) {
+        
+        Map templateModel = new HashMap();
+        templateModel.putAll(GeneratorProperties.getProperties());
+        templateModel.put("tableConfig", t);
+        setShareVars(templateModel);
+        
+        Map filePathModel = new HashMap();
+        filePathModel.putAll(GeneratorProperties.getProperties());
+        filePathModel.putAll(BeanHelper.describe(t));
+        setShareVars(filePathModel);
+        GeneratorModel gm = new GeneratorModel(templateModel,filePathModel);
+        return gm;
+    }
+    
+    private static void setShareVars(Map templateModel) {
+    	GeneratorModelUtils.setShareVars(templateModel);
+    }
+}
