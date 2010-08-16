2841c5fb154194a2f85d77b9396cf1f45b3004ae
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-08-16 06:51:03 +0000

    modify date format

--- trunk/rapid-framework4/log4jdbc/src-jdbc4/net/sf/log4jdbc/OracleRdbmsSpecifics.java
+++ trunk/rapid-framework4/log4jdbc/src-jdbc4/net/sf/log4jdbc/OracleRdbmsSpecifics.java
@@ -35,13 +35,13 @@ class OracleRdbmsSpecifics extends RdbmsSpecifics
   {
     if (object instanceof Timestamp)
     {
-      return "to_timestamp('" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS").
-        format(object) + "', 'mm/dd/yyyy hh24:mi:ss.ff3')";
+      return "to_timestamp('" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").
+        format(object) + "', 'yyyy-mm-dd hh24:mi:ss.ff3')";
     }
     else if (object instanceof Date)
     {
-      return "to_date('" + new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").
-        format(object) + "', 'mm/dd/yyyy hh24:mi:ss')";
+      return "to_date('" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").
+        format(object) + "', 'yyyy-mm-dd hh24:mi:ss')";
     }
     else
     {
