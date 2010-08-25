a2faebe81f65f004fe540015daf75fea920a6588
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-08-25 03:10:58 +0000

    add comments and sqlformat

--- trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/sql/model/Sql.java
+++ trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/provider/db/sql/model/Sql.java
@@ -4,13 +4,12 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 import cn.org.rapid_framework.generator.provider.db.sql.SqlFactory;
-import cn.org.rapid_framework.generator.provider.db.table.TableFactory;
-import cn.org.rapid_framework.generator.provider.db.table.TableFactory.DatabaseMetaDataUtils;
 import cn.org.rapid_framework.generator.provider.db.table.model.Column;
 import cn.org.rapid_framework.generator.provider.db.table.model.Table;
 import cn.org.rapid_framework.generator.util.StringHelper;
 import cn.org.rapid_framework.generator.util.sqlparse.SqlParseHelper;
 import cn.org.rapid_framework.generator.util.sqlparse.SqlTypeChecker;
+
 /**
  * 用于生成代码的Sql对象.对应数据库的sql语句
  * 使用SqlFactory.parseSql()生成 <br />
@@ -35,14 +34,14 @@ public class Sql {
 	public static String MULTIPLICITY_MANY = "many";
 	public static String MULTIPLICITY_PAGING = "paging";
 	
-	String tableSqlName = null; //是否需要
+	String                      tableSqlName        = null;                             // 是否需要
 	String operation = null;
 	String resultClass;
 	String parameterClass;
 	String remarks;
 	
 	String multiplicity = "many"; // many or one or paging
-	boolean paging = false; // 是否分页查询
+    boolean                     paging              = false;                            // 是否分页查询
 	String sqlmap; //for ibatis and ibatis3
 	
 	LinkedHashSet<Column> columns = new LinkedHashSet<Column>();
@@ -50,6 +49,7 @@ public class Sql {
 	
 	String sourceSql; // source sql
 	String executeSql;
+	/** primitive or object */
 	private String paramType = "object";
 	
 	public Sql() {
@@ -71,7 +71,7 @@ public class Sql {
 	}
 	
 	public boolean isColumnsInSameTable() {
-		//FIXME 还要增加表的列数与columns是否相等,才可以为select 生成 include语句
+		// FIXME 还要增加表的列数与columns是否相等,才可以为select 生成 include语句
 		if(columns == null || columns.isEmpty()) return false;
 		Column firstTable = columns.iterator().next();
 		if(columns.size() == 1) return true;
@@ -93,18 +93,18 @@ public class Sql {
 		}
 		return true;
 	}
-	
-	/**
-	 * 得到select查询返回的resultClass,可以通过setResultClass()自定义，如果没有自定义则为你自动生成<br />
-	 * resultClass可以为com.company.User的完全路径
-	 * 示例:
-	 * <pre>
-	 * select count(*) from user, 返回值为: Long
-	 * select * from user 返回值为: User
-	 * select count(*) cnt, sum(age) sum_age 返回值为: getOperation()+"Result";
-	 * </pre>
-	 * @return
-	 */
+
+    /**
+     * 得到select查询返回的resultClass,可以通过setResultClass()自定义，如果没有自定义则为你自动生成<br />
+     * resultClass可以为com.company.User的完全路径
+     * 示例:
+     * <pre>
+     * select count(*) from user, 返回值为: Long
+     * select * from user 返回值为: User
+     * select count(*) cnt, sum(age) sum_age 返回值为: getOperation()+"Result";
+     * </pre>
+     * @return
+     */
 	public String getResultClass() {
 		if(StringHelper.isNotBlank(resultClass)) return resultClass;
 		if(columns.size() == 1) {
@@ -121,25 +121,26 @@ public class Sql {
 	public void setResultClass(String queryResultClass) {
 		this.resultClass = queryResultClass;
 	}
-	/**
-	 * 返回getResultClass()的类名称 <br />
-	 * 示例: <br />
-	 * 如getResultClass()=com.company.User,将返回User
-	 */	
+
+    /**
+     * 返回getResultClass()的类名称 <br />
+     * 示例: <br />
+     * 如getResultClass()=com.company.User,将返回User
+     */	
 	public String getResultClassName() {
 		int lastIndexOf = getResultClass().lastIndexOf(".");
 		return lastIndexOf >= 0 ? getResultClass().substring(lastIndexOf+1) : getResultClass();
 	}
 
-	/**
-	 * SQL参数过多时用于封装为一个ParameterObject的class<br />
-	 * <pre>
-	 * 可以通过setParameterClass()自定义
-	 * 没有自定义则:
-	 * 如果是select查询,返回 operation+"Query"
-	 * 其它则返回operation+"Parameter"
-	 * <pre>
-	 */	
+    /**
+     * SQL参数过多时用于封装为一个ParameterObject的class<br />
+     * <pre>
+     * 可以通过setParameterClass()自定义
+     * 没有自定义则:
+     * 如果是select查询,返回 operation+"Query"
+     * 其它则返回operation+"Parameter"
+     * <pre>
+     */	
 	public String getParameterClass() {
 		if(StringHelper.isNotBlank(parameterClass)) return parameterClass;
 		if(StringHelper.isBlank(operation)) return null;
@@ -153,27 +154,29 @@ public class Sql {
 	public void setParameterClass(String parameterClass) {
 		this.parameterClass = parameterClass;
 	}
-	/**
-	 * 返回getParameterClass()的类名称 <br />
-	 * 示例: <br />
-	 * 如getParameterClass()=com.company.UserQuery,将返回UserQuery
-	 */		
+
+    /**
+     * 返回getParameterClass()的类名称 <br />
+     * 示例: <br />
+     * 如getParameterClass()=com.company.UserQuery,将返回UserQuery
+     */		
 	public String getParameterClassName() {
 		int lastIndexOf = getParameterClass().lastIndexOf(".");
 		return lastIndexOf >= 0 ? getParameterClass().substring(lastIndexOf+1) : getParameterClass();
 	}
 	
-	//TODO columnsSize大于二并且不是在同一张表中,将创建一个QueryResultClassName类,同一张表中也要考虑创建类
+	// TODO columnsSize大于二并且不是在同一张表中,将创建一个QueryResultClassName类,同一张表中也要考虑创建类
 	public int getColumnsCount() {
 		return columns.size();
 	}
 	public void addColumn(Column c) {
 		columns.add(c);
 	}
-	/**
-	 * 得到该sql方法相对应的操作名称,模板中的使用方式为: public List ${operation}(),示例值: findByUsername
-	 * @return
-	 */
+
+    /**
+     * 得到该sql方法相对应的操作名称,模板中的使用方式为: public List ${operation}(),示例值: findByUsername
+     * @return
+     */
 	public String getOperation() {
 		return operation;
 	}
@@ -183,41 +186,44 @@ public class Sql {
 	public String getOperationFirstUpper() {
 		return StringHelper.capitalize(getOperation());
 	}
-	/**
-	 * 用于控制查询结果,固定值为:one,many
-	 * @return
-	 */
+
+    /**
+     * 用于控制查询结果,固定值为:one,many
+     * @return
+     */
 	public String getMultiplicity() {
 		return multiplicity;
 	}
 	public void setMultiplicity(String multiplicity) {
-		//TODO 是否要增加验证数据为 one,many
+		// TODO 是否要增加验证数据为 one,many
 		this.multiplicity = multiplicity;
 	}
-	/**
-	 * 得到sqlect 查询的列对象(column),如果是insert,delete,update语句,则返回empty Set.<br />
-	 * 示例:
-	 * <pre>
-	 * SQL : select count(*) cnt, sum(age) sum_age from user_info
-	 * columns: cnt,sum_age
-	 * </pre>
-	 * @return
-	 */
+
+    /**
+     * 得到sqlect 查询的列对象(column),如果是insert,delete,update语句,则返回empty Set.<br />
+     * 示例:
+     * <pre>
+     * SQL : select count(*) cnt, sum(age) sum_age from user_info
+     * columns: cnt,sum_age
+     * </pre>
+     * @return
+     */
 	public LinkedHashSet<Column> getColumns() {
 		return columns;
 	}
 	public void setColumns(LinkedHashSet<Column> columns) {
 		this.columns = columns;
 	}
-	/**
-	 * 得到SQL的参数对象<br />
-	 * 示例:
-	 * <pre>
-	 * SQL : select * from user_info where username=:user and password=:pwd limit :offset,:limit
-	 * params: user,pwd,offset,limit
-	 * </pre>
-	 * @return
-	 */
+
+    /**
+     * 得到SQL的参数对象<br />
+     * 示例:
+     * <pre>
+     * SQL : select * from user_info where username=:user and password=:pwd limit :offset,:limit
+     * params: user,pwd,offset,limit
+     * </pre>
+     * @return
+     */
 	public LinkedHashSet<SqlParameter> getParams() {
 		return params;
 	}
@@ -232,10 +238,11 @@ public class Sql {
 		}
 		return null;
 	}
-	/**
-	 * 得到SQL原始语句
-	 * @return
-	 */
+
+    /**
+     * 得到SQL原始语句
+     * @return
+     */
 	public String getSourceSql() {
 		return sourceSql;
 	}
@@ -263,20 +270,20 @@ public class Sql {
 //				return o2.paramName.length() - o1.paramName.length();
 //			}
 //		});
-//		for(SqlParameter s : sortedParams){ //FIXME 现在只实现了:username参数替换
+    // for(SqlParameter s : sortedParams){ //FIXME 现在只实现了:username参数替换
 //			sql = StringHelper.replace(sql,":"+s.getParamName(),prefix+s.getParamName()+suffix);
 //		}
 //		return sql;
 //	}
-	/**
-	 * sourceSql转换为在数据库实际执行的SQL,
-	 * 示例:
-	 * <pre>
-	 * sourceSql: select * from user where username=:username and password=:password
-	 * executeSql: select * from user where username=? and password=?
-	 * </pre>
-	 * @return
-	 */
+    /**
+     * sourceSql转换为在数据库实际执行的SQL,
+     * 示例:
+     * <pre>
+     * sourceSql: select * from user where username=:username and password=:password
+     * executeSql: select * from user where username=? and password=?
+     * </pre>
+     * @return
+     */
 	public String getExecuteSql() {
 		return executeSql;
 	}
@@ -373,39 +380,43 @@ public class Sql {
 			return sql;
 		}
 	}
-	
-	/**
-	 * 当前的sourceSql是否是select语句
-	 * @return
-	 */
+
+    /**
+     * 当前的sourceSql是否是select语句
+     * @return
+     */
 	public boolean isSelectSql() {
 		return SqlTypeChecker.isSelectSql(sourceSql);
 	}
-	/**
-	 * 当前的sourceSql是否是update语句
-	 * @return
-	 */
+
+    /**
+     * 当前的sourceSql是否是update语句
+     * @return
+     */
 	public boolean isUpdateSql() {
 		return SqlTypeChecker.isUpdateSql(sourceSql);
 	}
-	/**
-	 * 当前的sourceSql是否是delete语句
-	 * @return
-	 */
+
+    /**
+     * 当前的sourceSql是否是delete语句
+     * @return
+     */
 	public boolean isDeleteSql() {
 		return SqlTypeChecker.isDeleteSql(sourceSql);
 	}
-	/**
-	 * 当前的sourceSql是否是insert语句
-	 * @return
-	 */
+
+    /**
+     * 当前的sourceSql是否是insert语句
+     * @return
+     */
 	public boolean isInsertSql() {
 		return SqlTypeChecker.isInsertSql(sourceSql);
 	}
-	/**
-	 * 得到表相对应的sqlName,主要用途为生成文件时的分组.
-	 * @return
-	 */
+
+    /**
+     * 得到表相对应的sqlName,主要用途为生成文件时的分组.
+     * @return
+     */
 	public String getTableSqlName() {
 		return tableSqlName;
 	}
@@ -413,10 +424,11 @@ public class Sql {
 	public void setTableSqlName(String tableName) {
 		this.tableSqlName = tableName;
 	}
-	/**
-	 * 得到备注
-	 * @return
-	 */
+
+    /**
+     * 得到备注
+     * @return
+     */
 	public String getRemarks() {
 		return remarks;
 	}
@@ -445,9 +457,9 @@ public class Sql {
     }
 
     /**
-	 * 根据tableSqlName和成相对应的tableClassName,主要用途路径变量引用.如${tableClassName}Dao.java
-	 * @return
-	 */
+     * 根据tableSqlName和成相对应的tableClassName,主要用途路径变量引用.如${tableClassName}Dao.java
+     * @return
+     */
 	public String getTableClassName() {
 		if(StringHelper.isBlank(tableSqlName)) return null;
 		String removedPrefixSqlName = Table.removeTableSqlNamePrefix(tableSqlName);
