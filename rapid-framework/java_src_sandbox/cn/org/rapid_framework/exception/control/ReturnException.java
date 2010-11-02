b894dcae6ca22f5d6fb233161e536baa0e4025f9
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-02 16:40:10 +0000

    delete return exception

--- rapid-framework/java_src_sandbox/cn/org/rapid_framework/exception/control/ReturnException.java
+++
@@ -1,6 +0,0 @@
-package cn.org.rapid_framework.exception.control;
-
-public class ReturnException extends RuntimeException {
-	private static final long serialVersionUID = 2522276124155174774L;
-	public Object result;
-}
