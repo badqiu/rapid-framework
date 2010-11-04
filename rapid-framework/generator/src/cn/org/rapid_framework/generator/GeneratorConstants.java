4d4b6d8bb271c8a14779a7591579e99e425cc87a
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-04 15:29:48 +0000

    引入GeneratorConstants

---
+++ rapid-framework/generator/src/cn/org/rapid_framework/generator/GeneratorConstants.java
@@ -0,0 +1,14 @@
+package cn.org.rapid_framework.generator;
+
+public interface GeneratorConstants {
+
+	/** 用于控制存放在生成器中的工具类,以便在模板中使用 StringUtils */
+	public String GENERATOR_TOOLS_CLASS = "generator.tools.class";
+	
+	public String GENERATOR_INCLUDES = "generator.includes";
+	public String GENERATOR_EXCLUDES = "generator.excludes";
+	public String GENERATOR_SOURCE_ENCODING = "generator.sourceEncoding";
+	public String GENERATOR_OUTPUT_ENCODING = "generator.outputEncoding";
+	public String GENERATOR_REMOVE_EXTENSIONS = "generator.removeExtensions";
+	
+}
