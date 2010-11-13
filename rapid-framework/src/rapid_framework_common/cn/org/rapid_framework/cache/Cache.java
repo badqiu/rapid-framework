313c95fb0c34f921290398f84038847bfc5e8e04
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-13 08:32:55 +0000

    add comments

--- rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/cache/Cache.java
+++ rapid-framework/src/rapid_framework_common/cn/org/rapid_framework/cache/Cache.java
@@ -4,20 +4,27 @@ import java.util.Map;
 
 /**
  * A cache interface
+ * 
+ * expiration的时间单位为秒
  * @see CacheHolder.cache
  */
 public interface Cache {
-
+	/** expiration的时间单位为秒 */
     public void add(String key, Object value, int expiration);
 
+    /** expiration的时间单位为秒 */
     public boolean safeAdd(String key, Object value, int expiration);
 
+    /** expiration的时间单位为秒 */
     public void set(String key, Object value, int expiration);
 
+    /** expiration的时间单位为秒 */
     public boolean safeSet(String key, Object value, int expiration);
 
+    /** expiration的时间单位为秒 */
     public void replace(String key, Object value, int expiration);
 
+    /** expiration的时间单位为秒 */
     public boolean safeReplace(String key, Object value, int expiration);
 
     public Object get(String key);
