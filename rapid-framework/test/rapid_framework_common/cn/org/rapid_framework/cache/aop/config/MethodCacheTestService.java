87016c258aa5e496b27cb9db87c854e9620e604a
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-15 11:34:44 +0000

    111

--- rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/cache/aop/config/MethodCacheTestService.java
+++ rapid-framework/test/rapid_framework_common/cn/org/rapid_framework/cache/aop/config/MethodCacheTestService.java
@@ -1,13 +1,12 @@
 package cn.org.rapid_framework.cache.aop.config;
 
-import java.util.concurrent.TimeUnit;
-
 import cn.org.rapid_framework.cache.aop.annotation.MethodCache;
 
 public class MethodCacheTestService {
 	
 	public int count = 100;
 	public int notCacheCount = 100;
+	public int ageCount = 100;
 	@MethodCache(expireTime=100)
 	public int queryByUser(String username,String password) {
 		return ++count;
@@ -16,4 +15,9 @@ public class MethodCacheTestService {
 	public int notCachedqueryByUser(String username,String password) {
 		return ++notCacheCount;
 	}
+	
+	@MethodCache(expireTime=100,cacheKey="Service.queryByUser(%s,%s,%s)")
+    public int queryByUser(String username,String password,int age) {
+        return ++ageCount;
+    }
 }
