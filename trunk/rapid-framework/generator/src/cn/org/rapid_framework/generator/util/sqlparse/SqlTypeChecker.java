211c79ce064caa7f1ff9fac37d4cea01ffe1cdfa
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-22 07:31:32 +0000

    add removeXMLCdataTag()

--- trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/util/sqlparse/SqlTypeChecker.java
+++ trunk/rapid-framework/generator/src/cn/org/rapid_framework/generator/util/sqlparse/SqlTypeChecker.java
@@ -1,5 +1,7 @@
 package cn.org.rapid_framework.generator.util.sqlparse;
 
+import cn.org.rapid_framework.generator.util.StringHelper;
+
 public class SqlTypeChecker {
 
 	/**
@@ -7,28 +9,28 @@ public class SqlTypeChecker {
 	 * @return
 	 */
 	public static boolean isSelectSql(String sourceSql) {
-		return SqlParseHelper.removeSqlComments(sourceSql).trim().toLowerCase().matches("(?is)\\s*select\\s.*\\sfrom\\s+.*");
+		return StringHelper.removeXMLCdataTag(SqlParseHelper.removeSqlComments(sourceSql)).trim().toLowerCase().matches("(?is)\\s*select\\s.*\\sfrom\\s+.*");
 	}
 	/**
 	 * 当前的sourceSql是否是update语句
 	 * @return
 	 */
 	public static boolean isUpdateSql(String sourceSql) {
-		return SqlParseHelper.removeSqlComments(sourceSql).trim().toLowerCase().matches("(?is)\\s*update\\s+.*");
+		return StringHelper.removeXMLCdataTag(SqlParseHelper.removeSqlComments(sourceSql)).trim().toLowerCase().matches("(?is)\\s*update\\s+.*");
 	}
 	/**
 	 * 当前的sourceSql是否是delete语句
 	 * @return
 	 */
 	public static boolean isDeleteSql(String sourceSql) {
-		return SqlParseHelper.removeSqlComments(sourceSql).trim().toLowerCase().matches("(?is)\\s*delete\\s+from\\s.*");
+		return StringHelper.removeXMLCdataTag(SqlParseHelper.removeSqlComments(sourceSql)).trim().toLowerCase().matches("(?is)\\s*delete\\s+from\\s.*");
 	}
 	/**
 	 * 当前的sourceSql是否是insert语句
 	 * @return
 	 */
 	public static boolean isInsertSql(String sourceSql) {
-		return SqlParseHelper.removeSqlComments(sourceSql).trim().toLowerCase().matches("(?is)\\s*insert\\s+into\\s+.*");
+		return StringHelper.removeXMLCdataTag(SqlParseHelper.removeSqlComments(sourceSql)).trim().toLowerCase().matches("(?is)\\s*insert\\s+into\\s+.*");
 	}
 	
 }
