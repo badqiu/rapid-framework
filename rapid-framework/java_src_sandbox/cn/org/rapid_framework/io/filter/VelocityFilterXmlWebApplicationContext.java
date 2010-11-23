68faf26f8c74b81023ca45e7ccc0384ad2fc74ad
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-23 12:58:21 +0000

    1

---
+++ rapid-framework/java_src_sandbox/cn/org/rapid_framework/io/filter/VelocityFilterXmlWebApplicationContext.java
@@ -0,0 +1,18 @@
+package cn.org.rapid_framework.io.filter;
+
+
+public class VelocityFilterXmlWebApplicationContext extends org.springframework.web.context.support.XmlWebApplicationContext{
+
+//    private VelocityFilterResourcePatternResolver resourceLoader = new VelocityFilterResourcePatternResolver();
+//    
+//    @Override
+//    public Resource getResource(String location) {
+//        return super.getResource(location);
+//    }
+//    
+//    @Override
+//    public Resource[] getResources(String locationPattern) throws IOException {
+//        return resourceLoader.getResources(locationPattern);
+//    }
+    
+}
