f68bdc3114710dc5a26be781bcacbe3d0771927d
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-28 12:27:27 +0000

    use ClassHelper.getDefaultClassLoader()

--- rapid-mvn/rapid-generator/rapid-generator/src/main/java/cn/org/rapid_framework/generator/util/ClassHelper.java
+++ rapid-mvn/rapid-generator/rapid-generator/src/main/java/cn/org/rapid_framework/generator/util/ClassHelper.java
@@ -1,5 +1,6 @@
 package cn.org.rapid_framework.generator.util;
 
+
 public class ClassHelper {
 
 	public static Object newInstance(Class<?> c) {
@@ -18,5 +19,32 @@ public class ClassHelper {
 			throw new IllegalArgumentException(
 					"cannot new instance with className:" + className, e);
 		}
+	}
+	
+	/**
+	 * Return the default ClassLoader to use: typically the thread context
+	 * ClassLoader, if available; the ClassLoader that loaded the ClassUtils
+	 * class will be used as fallback.
+	 * <p>Call this method if you intend to use the thread context ClassLoader
+	 * in a scenario where you absolutely need a non-null ClassLoader reference:
+	 * for example, for class path resource loading (but not necessarily for
+	 * <code>Class.forName</code>, which accepts a <code>null</code> ClassLoader
+	 * reference as well).
+	 * @return the default ClassLoader (never <code>null</code>)
+	 * @see java.lang.Thread#getContextClassLoader()
+	 */
+	public static ClassLoader getDefaultClassLoader() {
+		ClassLoader cl = null;
+		try {
+			cl = Thread.currentThread().getContextClassLoader();
+		}
+		catch (Throwable ex) {
+			// Cannot access thread context ClassLoader - falling back to system class loader...
+		}
+		if (cl == null) {
+			// No thread context class loader -> use class loader of this class.
+			cl = ClassHelper.class.getClassLoader();
+		}
+		return cl;
 	}
 }
