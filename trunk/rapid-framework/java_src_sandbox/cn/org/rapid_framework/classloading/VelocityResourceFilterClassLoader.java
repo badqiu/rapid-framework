a3b9a90138734d8ca4252b734be6614ef1c1b068
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-28 13:28:08 +0000

    1

---
+++ trunk/rapid-framework/java_src_sandbox/cn/org/rapid_framework/classloading/VelocityResourceFilterClassLoader.java
@@ -0,0 +1,92 @@
+package cn.org.rapid_framework.classloading;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.jar.Attributes;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.springframework.core.io.UrlResource;
+
+import cn.org.rapid_framework.io.filter.VelocityFilterResource;
+import cn.org.rapid_framework.io.filter.VelocityHelper;
+import cn.org.rapid_framework.jar.ManifestUtils;
+
+public class VelocityResourceFilterClassLoader extends ClassLoader {
+
+//	protected URL findResource(String name) {
+//		return super.findResource(name);
+//	}
+//
+//	protected Enumeration<URL> findResources(String name) throws IOException {
+//		return filterByVelocityIfRequired(super.findResources(name));
+//	}
+	
+	@Override
+	public URL getResource(String name) {
+		URL resource = super.getResource(name);
+		return filterByVelocityIfRequired(resource);
+	}
+
+	@Override
+	public InputStream getResourceAsStream(String name) {
+		InputStream in = super.getResourceAsStream(name);
+		return VelocityHelper.getInstance().evaluate(in);
+	}
+	
+	@Override
+	public Enumeration<URL> getResources(String name) throws IOException {
+		Enumeration<URL> urls = super.getResources(name);
+		return filterByVelocityIfRequired(urls);
+	}
+
+	private URL filterByVelocityIfRequired(URL resource) {
+		UrlResource decoratedResource = new UrlResource(resource);
+		try {
+			return new VelocityFilterResource(decoratedResource).getURL();
+		}catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private Enumeration<URL> filterByVelocityIfRequired(Enumeration<URL> urls) {
+		Vector<URL> list = new Vector<URL>();
+		while(urls.hasMoreElements()) {
+			list.add(filterByVelocityIfRequired(urls.nextElement()));
+		}
+		return list.elements();
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		Thread.currentThread().getContextClassLoader();
+		Thread.currentThread().setContextClassLoader(ClassLoader.getSystemClassLoader());
+		Package pkg = VelocityResourceFilterClassLoader.class.getPackage();
+		ClassLoader.getSystemClassLoader();
+		System.out.println(ToStringBuilder.reflectionToString(pkg));
+		printCodeSource(VelocityResourceFilterClassLoader.class);
+		printCodeSource(ApplicationAware.class);
+		printCodeSource(VelocityResourceFilterClassLoader.class);
+		
+		printManifest(ApplicationAware.class);
+		printManifest(ApplicationAware.class);
+		printManifest(VelocityResourceFilterClassLoader.class);
+	}
+
+	private static void printCodeSource(Class clazz) {
+		CodeSource codeSource = clazz.getProtectionDomain().getCodeSource();
+		System.out.println(codeSource.getLocation());
+		System.out.println(codeSource.getCertificates());
+		System.out.println(codeSource.getCodeSigners());
+	}
+	
+	private static void printManifest(Class clazz) {
+		Attributes attrs = ManifestUtils.getManifest(clazz).getMainAttributes();
+		System.out.println("manifest:"+Arrays.toString(attrs.values().toArray()));
+	}
+}
