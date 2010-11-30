42dd8ba3603808a9ebdbdeccf299105d066fc0e6
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-11-30 16:22:58 +0000

    delete templateRootDir

--- rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/demo/GenerateBySqlDemoTest.java
+++ rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/demo/GenerateBySqlDemoTest.java
@@ -1,5 +1,7 @@
 package cn.org.rapid_framework.generator.demo;
 
+import java.io.File;
+
 import cn.org.rapid_framework.generator.GeneratorFacade;
 import cn.org.rapid_framework.generator.GeneratorTestCase;
 import cn.org.rapid_framework.generator.provider.db.sql.SqlFactory;
@@ -10,15 +12,16 @@ public class GenerateBySqlDemoTest extends GeneratorTestCase{
 	public void setUp() throws Exception {
 		super.setUp();
 		g.setOutRootDir("temp/"+getClass().getSimpleName()+"/"+getName());
-		generatorFacade.g = g;
+		generatorFacade.setGenerator(g);
 	}
 	
 	public void test_generate_by_sql() throws Exception {
+		generatorFacade.getGenerator().addTemplateRootDir(new File("for_test_select_sql"));
 		Sql sql = new SqlFactory().parseSql("select * from user_info where username=#username# and password=#password#");
 		sql.setMultiplicity("many");  //many or one
 		sql.setOperation("findByUsernameAndPassword");
 		sql.setRemarks("根据用户名及密码进行查询");
-		generatorFacade.generateBySql(sql, "for_test_select_sql");
+		generatorFacade.generateBySql(sql);
 		
 	}
 	
