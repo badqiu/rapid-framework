089ff0efc9de329b0493b33e5569eebd47aec25f
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-01 05:54:34 +0000

    1

--- trunk/rapid-mvn/rapid-core/src/main/java/cn/org/rapid_framework/util/Asserter.java
+++ trunk/rapid-mvn/rapid-core/src/main/java/cn/org/rapid_framework/util/Asserter.java
@@ -21,7 +21,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.isTrue(i &gt; 0, "The value must be greater than zero");</pre>
+	 * <pre class="code">Asserter.isTrue(age &gt; 0, new IllegalBizArgumentsException(ErrorCode.AGE_ERROR));</pre>
 	 * @param expression a boolean expression
 	 * @param throwIfAssertFail 
 	 */
@@ -33,7 +33,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.isNull(value, "The value must be null");</pre>
+	 * <pre class="code">Asserter.isNull(username, new IllegalBizArgumentsException(ErrorCode.USERNAME_ERROR));</pre>
 	 * @param object the object to check
 	 * @param throwIfAssertFail 
 	 */
@@ -45,7 +45,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.notNull(clazz, "The class must not be null");</pre>
+	 * <pre class="code">Asserter.notNull(username, new IllegalBizArgumentsException(ErrorCode.USERNAME_ERROR));</pre>
 	 * @param object the object to check
 	 * @param throwIfAssertFail 
 	 */
@@ -57,7 +57,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.hasLength(name, "Name must not be empty");</pre>
+	 * <pre class="code">Asserter.notEmpty(username, new IllegalBizArgumentsException(ErrorCode.USERNAME_ERROR));</pre>
 	 * @param text the String to check
 	 * @param throwIfAssertFail 
 	 * @see StringUtils#hasLength
@@ -70,7 +70,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.hasText(name, "'name' must not be empty");</pre>
+	 * <pre class="code">Asserter.notBlank(username, new IllegalBizArgumentsException(ErrorCode.USERNAME_ERROR));</pre>
 	 * @param text the String to check
 	 * @param throwIfAssertFail 
 	 * @see StringUtils#hasText
@@ -83,7 +83,7 @@ public class Asserter {
 
 	/**
 	 * 抛出指定的异常,如果assert fail.
-	 * <pre class="code">Asserter.doesNotContain(name, "rod", "Name must not contain 'rod'");</pre>
+	 * <pre class="code">Asserter.doesNotContain(name, "rod",, new IllegalBizArgumentsException(ErrorCode.NAME_MUST_NOT_CONTAIN_ROD_ERROR));</pre>
 	 * @param textToSearch the text to search
 	 * @param substring the substring to find within the text
 	 * @param throwIfAssertFail 
@@ -100,7 +100,7 @@ public class Asserter {
 	 * 抛出指定的异常,如果assert fail.
 	 * Assert that an array has elements; that is, it must not be
 	 * <code>null</code> and must have at least one element.
-	 * <pre class="code">Asserter.notEmpty(array, "The array must have elements");</pre>
+	 * <pre class="code">Asserter.notEmpty(array, new IllegalBizArgumentsException(ErrorCode.ARRAY_MUST_HAVE_ELEMENTS_ERROR));</pre>
 	 * @param array the array to check
 	 * @param throwIfAssertFail 
 	 */
@@ -114,7 +114,7 @@ public class Asserter {
 	 * 抛出指定的异常,如果assert fail.
 	 * Assert that an array has no null elements.
 	 * Note: Does not complain if the array is empty!
-	 * <pre class="code">Asserter.noNullElements(array, new BlogException("The array must have non-null elements"));</pre>
+	 * <pre class="code">Asserter.noNullElements(array, new IllegalBizArgumentsException(ErrorCode.ARRAY_MUST_HAVE_NOT_NULL_ELEMENTS_ERROR));</pre>
 	 * @param array the array to check
 	 * @param throwIfAssertFail 
 	 */
@@ -132,7 +132,7 @@ public class Asserter {
 	 * 抛出指定的异常,如果assert fail.
 	 * Assert that a collection has elements; that is, it must not be
 	 * <code>null</code> and must have at least one element.
-	 * <pre class="code">Asserter.notEmpty(collection, "Collection must have elements");</pre>
+	 * <pre class="code">Asserter.notEmpty(userList, new IllegalBizArgumentsException(ErrorCode.USER_LIST_MUST_NOT_EMPTY_ERROR));</pre>
 	 * @param collection the collection to check
 	 * @param throwIfAssertFail 
 	 */
@@ -146,7 +146,7 @@ public class Asserter {
 	 * 抛出指定的异常,如果assert fail.
 	 * Assert that a Map has entries; that is, it must not be <code>null</code>
 	 * and must have at least one entry.
-	 * <pre class="code">Asserter.notEmpty(map, "Map must have entries");</pre>
+	 * <pre class="code">Asserter.notEmpty(userMap, new IllegalBizArgumentsException(ErrorCode.USER_MAP_MUST_NOT_EMPTY_ERROR));</pre>
 	 * @param map the map to check
 	 * @param throwIfAssertFail 
 	 */
