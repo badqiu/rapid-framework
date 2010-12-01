2930ce19d6098beb8007e71ca24acb043e74479b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-01 08:47:28 +0000

    1

--- trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigSet.java
+++ trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigSet.java
@@ -1,5 +1,6 @@
 package cn.org.rapid_framework.generator.ext.tableconfig.model;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -8,7 +9,18 @@ import cn.org.rapid_framework.generator.util.StringHelper;
 
 public class TableConfigSet implements Iterable<TableConfig>{
 	private Set<TableConfig> tableConfigs = new LinkedHashSet<TableConfig>();
-	
+    
+	private String _package;
+    
+    public String getPackage() {
+        return _package;
+    }
+
+    public void setPackage(String _package) {
+        this._package = _package;
+        setPackageForTableConfigs();
+    }
+    
 	public Set<String> getSequences() {
 		Set<String> result = new LinkedHashSet<String>();
 		for(TableConfig c : tableConfigs) {
@@ -20,16 +32,24 @@ public class TableConfigSet implements Iterable<TableConfig>{
 	}
 	
 	public void addTableConfig(TableConfig t) {
+	    t.setPackage(getPackage());
 		tableConfigs.add(t);
 	}
 	
 	public Set<TableConfig> getTableConfigs() {
-		return tableConfigs;
+		return Collections.unmodifiableSet(tableConfigs);
 	}
 
 	public void setTableConfigs(Set<TableConfig> tableConfigs) {
 		this.tableConfigs = tableConfigs;
+		setPackageForTableConfigs();
 	}
+
+    private void setPackageForTableConfigs() {
+        for(TableConfig t : this.tableConfigs) {
+		    t.setPackage(getPackage());
+		}
+    }
 
 	public TableConfig getBySqlName(String sqlName) {
 		for(TableConfig c : tableConfigs) {
