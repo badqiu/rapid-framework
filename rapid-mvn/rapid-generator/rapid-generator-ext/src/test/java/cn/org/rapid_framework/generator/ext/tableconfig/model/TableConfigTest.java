eaff24fb1e37448a1355d953f4839defd1263a4b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-01 12:05:05 +0000

    1

--- rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigTest.java
+++ rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigTest.java
@@ -1,11 +1,37 @@
 package cn.org.rapid_framework.generator.ext.tableconfig.model;
 
 import junit.framework.TestCase;
+import cn.org.rapid_framework.generator.GeneratorConstants;
+import cn.org.rapid_framework.generator.GeneratorProperties;
 
 public class TableConfigTest extends TestCase {
     
     public void test() {
         TableConfig tc = new TableConfig();
         tc.setSqlname("user_info");
+        assertEquals(tc.getClassName(),"UserInfo");
+        
+        GeneratorProperties.setProperty(GeneratorConstants.TABLE_NAME_SINGULARIZE, "true");
+        TableConfig table = new TableConfig();
+        table.setSqlname("bashes");
+        assertEquals("Bashe",table.getClassName());
+        
+        table.setSqlname("cuStomeRs");
+        assertEquals("CuStomeR",table.getClassName());
+        
+        GeneratorProperties.setProperty(GeneratorConstants.TABLE_REMOVE_PREFIXES, "t_,v_");
+        table.setSqlname("t_user_infos");
+        assertEquals("UserInfo",table.getClassName());
+        
+        GeneratorProperties.setProperty(GeneratorConstants.TABLE_NAME_SINGULARIZE, "false");
+        table.setSqlname("bashes");
+        assertEquals("Bashes",table.getClassName());
+        
+        table.setSqlname("cuStomeRs");
+        assertEquals("CuStomeRs",table.getClassName());
+        
+        GeneratorProperties.setProperty(GeneratorConstants.TABLE_REMOVE_PREFIXES, "t_,v_");
+        table.setSqlname("t_user_infos");
+        assertEquals("UserInfos",table.getClassName());
     }
 }
