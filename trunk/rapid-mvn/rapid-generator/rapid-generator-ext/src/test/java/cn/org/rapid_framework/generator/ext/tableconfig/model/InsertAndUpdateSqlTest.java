61290710cbfb063aa8067aca60af30dd8462cd6b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-04 12:00:21 +0000

    1

--- trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/InsertAndUpdateSqlTest.java
+++ trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/InsertAndUpdateSqlTest.java
@@ -9,7 +9,7 @@ public class InsertAndUpdateSqlTest extends GeneratorTestCase{
 		testDbType = "mysql";
 	}
 	
-	public void test() {
+	public void test_insert() {
 		new SqlFactory().parseSql("insert into user_info (user_id) values (99999999999999999999999999999999)");
 		new SqlFactory().parseSql("insert into user_info (user_id,password) values (9999998,'9999999999999999999999999999999999999999999999999999999999999999999999999999999')");
 		new SqlFactory().parseSql("insert into user_info (user_id,password) values (123,'123')");
@@ -22,7 +22,26 @@ public class InsertAndUpdateSqlTest extends GeneratorTestCase{
 		
 		try {
 		new SqlFactory().parseSql("insert into user_info (user_id,birth_date) values (5555555555,'1999')");
-		fail();
+		fail("错误的日期格式");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void test_update() {
+		new SqlFactory().parseSql("update user_info set user_id = 99999999999999999999999999999999");
+		new SqlFactory().parseSql("update user_info set user_id = 9999998 ,password = '9999999999999999999999999999999999999999999999999999999999999999999999999999999' ");
+		new SqlFactory().parseSql("update user_info set user_id = 123,password = '123'");
+		new SqlFactory().parseSql("update user_info set user_id = 5555555555 , birth_date = '1999-10-10'");
+		
+		new SqlFactory().parseSql("update role_permission set  role_id = 9525,permissoin_id=9525");
+		new SqlFactory().parseSql("update role_permission set  role_id = 123,permissoin_id = 9525");
+		
+		new SqlFactory().parseSql("update role_permission set role_id = 9525,permissoin_id=9525");
+		
+		try {
+		new SqlFactory().parseSql("update user_info set (user_id = 5555555555,birth_date = 1999)");
+		fail("错误的日期格式");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
