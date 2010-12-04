f4f92c4dc3ddf6f1ec1710c27fde0cdf8534daad
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-04 13:42:37 +0000

    1

--- trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/InsertAndUpdateSqlTest.java
+++ trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/InsertAndUpdateSqlTest.java
@@ -1,7 +1,9 @@
 package cn.org.rapid_framework.generator.ext.tableconfig.model;
 
 import cn.org.rapid_framework.generator.GeneratorTestCase;
+import cn.org.rapid_framework.generator.provider.db.DataSourceProvider;
 import cn.org.rapid_framework.generator.provider.db.sql.SqlFactory;
+import cn.org.rapid_framework.generator.util.SqlExecutorHelper;
 
 public class InsertAndUpdateSqlTest extends GeneratorTestCase{
 
@@ -29,21 +31,33 @@ public class InsertAndUpdateSqlTest extends GeneratorTestCase{
 	}
 	
 	public void test_update() {
-		new SqlFactory().parseSql("update user_info set user_id = 99999999999999999999999999999999");
-		new SqlFactory().parseSql("update user_info set user_id = 9999998 ,password = '9999999999999999999999999999999999999999999999999999999999999999999999999999999' ");
-		new SqlFactory().parseSql("update user_info set user_id = 123,password = '123'");
-		new SqlFactory().parseSql("update user_info set user_id = 5555555555 , birth_date = '1999-10-10'");
-		
-		new SqlFactory().parseSql("update role_permission set  role_id = 9525,permissoin_id=9525");
-		new SqlFactory().parseSql("update role_permission set  role_id = 123,permissoin_id = 9525");
-		
-		new SqlFactory().parseSql("update role_permission set role_id = 9525,permissoin_id=9525");
-		
 		try {
-		new SqlFactory().parseSql("update user_info set (user_id = 5555555555,birth_date = 1999)");
-		fail("错误的日期格式");
-		}catch(Exception e) {
-			e.printStackTrace();
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "insert into user_info (user_id,password) values (1111,'2222')");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "insert into user_info (user_id,password) values (2222,'2222')");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "insert into role (role_id,created,modified) values (9999,now(),now())");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "insert into permission (permissoin_id,created,modified) values (9999,now(),now())");
+			
+			new SqlFactory().parseSql("update user_info set user_id = 99999999999999999999999999999999");
+			new SqlFactory().parseSql("update user_info set user_id = 9999998 ,password = '9999999999999999999999999999999999999999999999999999999999999999999999999999999' ");
+			new SqlFactory().parseSql("update user_info set user_id = 123,password = '123'");
+			new SqlFactory().parseSql("update user_info set user_id = 5555555555 , birth_date = '1999-10-10'");
+			
+			new SqlFactory().parseSql("update role_permission set  role_id = 9525,permissoin_id=9525");
+			new SqlFactory().parseSql("update role_permission set  role_id = 123,permissoin_id = 9525");
+			
+			new SqlFactory().parseSql("update role_permission set role_id = 9525,permissoin_id=9525");
+			
+			try {
+			new SqlFactory().parseSql("update user_info set (user_id = 5555555555,birth_date = 1999)");
+			fail("错误的日期格式");
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}finally {
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "delete from user_info where user_id = 1111");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "delete from user_info where user_id = 2222");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "delete from role where role_id = 9999");
+			SqlExecutorHelper.execute(DataSourceProvider.getDataSource(), "delete from permission where permissoin_id = 9999");
 		}
 	}
 }
