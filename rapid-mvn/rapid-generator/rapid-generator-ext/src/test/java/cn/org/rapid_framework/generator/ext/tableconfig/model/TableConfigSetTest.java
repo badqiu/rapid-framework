c89fe2410334a4d89b8b405d1d07723629fe19c9
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-07 16:18:51 +0000

    add TableConfigSet test

--- rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigSetTest.java
+++ rapid-mvn/rapid-generator/rapid-generator-ext/src/test/java/cn/org/rapid_framework/generator/ext/tableconfig/model/TableConfigSetTest.java
@@ -1,8 +1,15 @@
 package cn.org.rapid_framework.generator.ext.tableconfig.model;
 
-import junit.framework.TestCase;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
-public class TableConfigSetTest extends TestCase {
+import junit.framework.TestCase;
+import cn.org.rapid_framework.generator.GeneratorTestCase;
+import cn.org.rapid_framework.generator.ext.tableconfig.model.TableConfig.OperationConfig;
+import cn.org.rapid_framework.generator.provider.db.sql.model.Sql;
+
+public class TableConfigSetTest extends GeneratorTestCase {
     
     public void test() {
         TableConfigSet set = new TableConfigSet();
@@ -15,4 +22,36 @@ public class TableConfigSetTest extends TestCase {
         config.setSequence("123");
         assertFalse(set.getSequences().isEmpty());
     }
+    Sql sql;
+    public void testListner() throws SQLException, Exception {
+//    	sql = newSql(newOperation("queryUserInfo","select * from user_info"),null);
+//		
+//		assertEquals(sql.getResultClass(),"UserInfo");
+		
+    	sql = newSql(newOperation("queryUserInfo","select * from user_info"),"CustomUserInfo");
+		
+		assertEquals(sql.getResultClass(),"CustomUserInfo");
+		
+    }
+    
+    public OperationConfig newOperation(String name,String sql) {
+    	OperationConfig operation = new OperationConfig();
+    	operation.setSql(sql);
+    	operation.setName(name);
+    	return operation;
+    }
+
+	private Sql newSql(OperationConfig operation,String customClassName)
+			throws SQLException, Exception {
+		TableConfigSet set = new TableConfigSet();
+    	TableConfig tc = new TableConfig();
+    	tc.setSqlName("user_info");
+    	tc.setClassName(customClassName);
+		tc.setOperations(Arrays.asList(operation));
+		set.addTableConfig(tc);
+		
+		List<Sql> sqls = tc.getSqls();
+		Sql sql = sqls.get(0);
+		return sql;
+	}
 }
