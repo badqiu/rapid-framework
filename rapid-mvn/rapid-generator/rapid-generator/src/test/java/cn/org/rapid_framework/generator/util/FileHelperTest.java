1f98486b23d97a0b916b90f8202bdcd62f375ad5
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-09 17:23:15 +0000

    1

--- rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/util/FileHelperTest.java
+++ rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/util/FileHelperTest.java
@@ -3,6 +3,8 @@ package cn.org.rapid_framework.generator.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import junit.framework.TestCase;
 
@@ -16,5 +18,11 @@ public class FileHelperTest extends TestCase {
     public void test() {
     	assertEquals(FileHelper.toFilePathIfIsURL(new File("file://c:/123.txt")),"/c:/123.txt");
     	assertEquals(FileHelper.toFilePathIfIsURL(new File("c:/123456.txt")).replace('/', '\\'),"c:\\123456.txt");
+	
+		try {
+			new URL("c://123456.txt");
+			fail();
+		} catch (MalformedURLException e) {
+		}
     }
 }
