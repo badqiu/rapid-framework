fd13dbcbbdce720a52b0e996bf2365565a871aa6
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-10 06:34:14 +0000

    add FileHelper.getFile()

--- rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/util/FileHelperTest.java
+++ rapid-mvn/rapid-generator/rapid-generator/src/test/java/cn/org/rapid_framework/generator/util/FileHelperTest.java
@@ -15,7 +15,7 @@ public class FileHelperTest extends TestCase {
         assertTrue(FileHelper.isBinaryFile(new File(".zip")));
     }
     
-    public void test() {
+    public void test_toFilePathIfIsURL() {
     	assertEquals(FileHelper.toFilePathIfIsURL(new File("file://c:/123.txt")),"/c:/123.txt");
     	assertEquals(FileHelper.toFilePathIfIsURL(new File("c:/123456.txt")).replace('/', '\\'),"c:\\123456.txt");
 	
@@ -24,5 +24,39 @@ public class FileHelperTest extends TestCase {
 			fail();
 		} catch (MalformedURLException e) {
 		}
+    }
+    
+    String userHome = System.getProperty("user.home");
+    String tempDir = System.getProperty("java.io.tmpdir");
+    public void test_getFile() {
+        FileHelper.getFile("classpath:cn/org/rapid_framework/generator/util/FileHelperTest.class");
+        
+        try {
+            FileHelper.getFile("classpath:cn/org/rapid_framework/generator/util/FileHelperTest_not_exist.class");
+            fail("not found file");
+        }catch(RuntimeException e){
+        }
+        
+        File file = FileHelper.getFile("pom.xml");
+        assertTrue(file.exists());
+        
+        new File(tempDir,"test_getFile").mkdir();
+        new File(tempDir,"test_getFile").deleteOnExit();
+        
+        file = FileHelper.getFile(tempDir+"/test_getFile");
+        assertTrue(file.getAbsolutePath(),file.exists());
+        
+        file = FileHelper.getFile("file:"+tempDir+"/test_getFile");
+        assertTrue(file.getAbsolutePath(),file.exists());
+        
+        file = FileHelper.getFile("file://"+tempDir+"/test_getFile");
+        assertTrue(file.getAbsolutePath(),file.exists());
+        
+        try {
+            file = FileHelper.getFile("classpath://"+tempDir+"/test_getFile");
+            fail("not found file");
+        }catch(Exception e) {
+        }
+        
     }
 }
