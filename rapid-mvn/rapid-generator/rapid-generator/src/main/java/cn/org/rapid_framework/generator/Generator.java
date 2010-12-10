b1e60d0eecf84bd4909d71d582130b053ee1862a
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-10 15:55:22 +0000

    修改为generatorByTableName(String... tableNames)

--- rapid-mvn/rapid-generator/rapid-generator/src/main/java/cn/org/rapid_framework/generator/Generator.java
+++ rapid-mvn/rapid-generator/rapid-generator/src/main/java/cn/org/rapid_framework/generator/Generator.java
@@ -511,17 +511,20 @@ public class Generator  {
 			return list;
 		}
 	}
-
+	@SuppressWarnings("unchecked")
 	public static class GeneratorModel implements java.io.Serializable{
-	    /** 用于存放'模板'可以引用的变量 */
+		private static final long serialVersionUID = -6430787906037836995L;
+		/** 用于存放'模板'可以引用的变量 */
 		public Map templateModel = new HashMap();
 		/** 用于存放'文件路径'可以引用的变量 */
 		public Map filePathModel = new HashMap();
+		
 		public GeneratorModel() {
 		}
 		public GeneratorModel(Map templateModel, Map filePathModel) {
 			this.templateModel = templateModel;
 			this.filePathModel = filePathModel;
 		}
+		
 	}
 }
