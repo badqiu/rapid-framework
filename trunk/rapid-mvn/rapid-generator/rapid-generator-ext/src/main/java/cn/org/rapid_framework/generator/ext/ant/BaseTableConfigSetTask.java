fce04f39fe395d0cfe15cdea0bd704662a07e11f
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-11 03:52:19 +0000

    add ThreadLocalCache

--- trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/ant/BaseTableConfigSetTask.java
+++ trunk/rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/ant/BaseTableConfigSetTask.java
@@ -2,6 +2,8 @@ package cn.org.rapid_framework.generator.ext.ant;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.org.rapid_framework.generator.ext.tableconfig.builder.TableConfigXmlBuilder;
 import cn.org.rapid_framework.generator.ext.tableconfig.model.TableConfigSet;
@@ -17,27 +19,39 @@ public abstract class BaseTableConfigSetTask extends BaseGeneratorTask{
 	private String tableConfigFiles; 
 	protected TableConfigSet tableConfigSet;
 	
+	private static ThreadLocal<Map> threadLocalCache = new ThreadLocal<Map>();
 
 	@Override
 	protected void executeBefore() throws Exception {
 		super.executeBefore();
 		
-		if(tableConfigSet == null) {
-			tableConfigSet = parseForTableConfigSet(getPackage(), getProject().getBaseDir().getAbsoluteFile(), tableConfigFiles);
+		if(tableConfigFiles == null || "".equals(tableConfigFiles.trim())) {
+			throw new Exception("'tableConfigFiles' must be not null");
 		}
 		
 		if(tableConfigSet == null) {
-			throw new Exception("'tableConfigFiles' or 'tableConfigSet' must be not null");
+			Map cache = getThreadLocalCache();
+			tableConfigSet = (TableConfigSet)cache.get(tableConfigFiles);
+			if(tableConfigSet == null) {
+				tableConfigSet = parseForTableConfigSet(getPackage(), getProject().getBaseDir().getAbsoluteFile(), tableConfigFiles);
+			}
+			cache.put(tableConfigFiles, tableConfigSet);
 		}
+	}
+
+	public static Map getThreadLocalCache() {
+		Map map = threadLocalCache.get();
+		if(map == null) {
+			map = new HashMap();
+			threadLocalCache.set(map);
+		}
+		return map;
 	}
 
 	public void setTableConfigFiles(String tableConfigFiles) {
 		this.tableConfigFiles = tableConfigFiles;
 	}
 
-	public void setTableConfigSet(TableConfigSet tableConfigSet) {
-		this.tableConfigSet = tableConfigSet;
-	}
 	
     static TableConfigSet parseForTableConfigSet(String _package,File basedir,String tableConfigFiles) {
     	String[] tableConfigFilesArray = StringHelper.tokenizeToStringArray(tableConfigFiles, ", \t\n\r\f");
