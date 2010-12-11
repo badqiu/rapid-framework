b577756a650947fac2d996d6391d4595025f42c5
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-11 11:21:32 +0000

    initial import

---
+++ rapid-mvn/rapid-generator/rapid-generator-template/src/main/resources/generator/template/rapid/table/dao_spring_jdbc/src/main/java/${basepackage_dir}/model/${className}.java
@@ -0,0 +1,143 @@
+<#include "/macro.include"/>
+<#include "/java_copyright.include">
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first> 
+package ${basepackage}.model;
+
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.*;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import javax.persistence.*;
+
+<#include "/java_imports.include">
+
+public class ${className} extends BaseEntity implements java.io.Serializable{
+	private static final long serialVersionUID = 5454155825314635342L;
+	
+	//alias
+	public static final String TABLE_ALIAS = "${table.tableAlias}";
+	<#list table.columns as column>
+	public static final String ALIAS_${column.constantName} = "${column.columnAlias}";
+	</#list>
+	
+	//date formats
+	<#list table.columns as column>
+	<#if column.isDateTimeColumn>
+	public static final String FORMAT_${column.constantName} = DATE_FORMAT;
+	</#if>
+	</#list>
+	
+	//可以直接使用: @Length(max=50,message="用户名长度不能大于50")显示错误消息
+	//columns START
+	<#list table.columns as column>
+    /**
+     * ${column.columnAlias!}       db_column: ${column.sqlName} 
+     */ 	
+	${column.hibernateValidatorExprssion}
+	private ${column.javaType} ${column.columnNameLower};
+	</#list>
+	//columns END
+	
+	//注意： spring_jdbc的MetadataCreateUtils.fromTable(Entity.class) 可以读取JPA annotation的标注信息
+	//现支持 @Id,@Column,@Table标注
+
+<@generateConstructor className/>
+<@generateJavaColumns/>
+<@generateJavaOneToMany/>
+<@generateJavaManyToOne/>
+
+	public String toString() {
+		return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+		<#list table.columns as column>
+			.append("${column.columnName}",get${column.columnName}())
+		</#list>
+			.toString();
+	}
+	
+	public int hashCode() {
+		return new HashCodeBuilder()
+		<#list table.pkColumns as column>
+			.append(get${column.columnName}())
+		</#list>
+			.toHashCode();
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof ${className} == false) return false;
+		if(this == obj) return true;
+		${className} other = (${className})obj;
+		return new EqualsBuilder()
+			<#list table.pkColumns as column>
+			.append(get${column.columnName}(),other.get${column.columnName}())
+			</#list>
+			.isEquals();
+	}
+}
+
+<#macro generateJavaColumns>
+	<#list table.columns as column>
+		<#if column.pk>
+	@Id
+		</#if>	
+		<#if column.isDateTimeColumn>
+	@Transient
+	public String get${column.columnName}String() {
+		return DateConvertUtils.format(get${column.columnName}(), FORMAT_${column.constantName});
+	}
+	public void set${column.columnName}String(String value) {
+		set${column.columnName}(DateConvertUtils.parse(value, FORMAT_${column.constantName},${column.javaType}.class));
+	}
+	
+		</#if>	
+	public ${column.javaType} get${column.columnName}() {
+		return this.${column.columnNameLower};
+	}
+	
+	public void set${column.columnName}(${column.javaType} value) {
+		this.${column.columnNameLower} = value;
+	}
+	
+	</#list>
+</#macro>
+
+
+<#macro generateJavaOneToMany>
+	<#list table.exportedKeys.associatedTables?values as foreignKey>
+	<#assign fkSqlTable = foreignKey.sqlTable>
+	<#assign fkTable    = fkSqlTable.className>
+	<#assign fkPojoClass = fkSqlTable.className>
+	<#assign fkPojoClassVar = fkPojoClass?uncap_first>
+	
+	private Set ${fkPojoClassVar}s = new HashSet(0);
+	@Transient
+	public Set<${fkPojoClass}> get${fkPojoClass}s() {
+		return ${fkPojoClassVar}s;
+	}	
+	public void set${fkPojoClass}s(Set<${fkPojoClass}> ${fkPojoClassVar}){
+		this.${fkPojoClassVar}s = ${fkPojoClassVar};
+	}
+	</#list>
+</#macro>
+
+<#macro generateJavaManyToOne>
+	<#list table.importedKeys.associatedTables?values as foreignKey>
+	<#assign fkSqlTable = foreignKey.sqlTable>
+	<#assign fkTable    = fkSqlTable.className>
+	<#assign fkPojoClass = fkSqlTable.className>
+	<#assign fkPojoClassVar = fkPojoClass?uncap_first>
+	
+	private ${fkPojoClass} ${fkPojoClassVar};
+	@Transient
+	public ${fkPojoClass} get${fkPojoClass}() {
+		return ${fkPojoClassVar};
+	}	
+	public void set${fkPojoClass}(${fkPojoClass} ${fkPojoClassVar}){
+		this.${fkPojoClassVar} = ${fkPojoClassVar};
+	}
+	</#list>
+</#macro>
+
---
+++ rapid-mvn/rapid-generator/rapid-generator-template/src/main/resources/generator/template/rapid/table/ria_extjs/src/readme.txt
@@ -0,0 +1,3 @@
+${gg.setIgnoreOutput(true)}
+
+Rich Internet Application (RIA) with extjs
