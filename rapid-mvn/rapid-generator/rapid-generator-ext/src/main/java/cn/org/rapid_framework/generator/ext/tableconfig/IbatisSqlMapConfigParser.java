077c11dacc73e4764aac77ad0222e1c0c0fd44b4
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-14 12:08:35 +0000

    重构

--- rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/tableconfig/IbatisSqlMapConfigParser.java
+++ rapid-mvn/rapid-generator/rapid-generator-ext/src/main/java/cn/org/rapid_framework/generator/ext/tableconfig/IbatisSqlMapConfigParser.java
@@ -1,14 +1,10 @@
 package cn.org.rapid_framework.generator.ext.tableconfig;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import cn.org.rapid_framework.generator.util.FileHelper;
-import cn.org.rapid_framework.generator.util.IOHelper;
 import cn.org.rapid_framework.generator.util.StringHelper;
 import cn.org.rapid_framework.generator.util.XMLHelper;
 import cn.org.rapid_framework.generator.util.sqlparse.SqlParseHelper;
@@ -21,14 +17,15 @@ import cn.org.rapid_framework.generator.util.sqlparse.SqlParseHelper;
  * 
  */
 public class IbatisSqlMapConfigParser {
-
+    private Map usedIncludedSqls = new HashMap(); //增加一条sql语句引用的 includesSql的解析
+    
     public String parse(String str) {
         return parse(str,new HashMap());
     }	
 
     public String parse(String str,Map<String,String> includeSqls) {
-        str = removeComments("<for_remove_comment>"+str+"</for_remove_comment>");
-        str = removeSelectKeyXmlForInsertSql(str);
+        str = Helper.removeComments("<for_remove_comment>"+str+"</for_remove_comment>");
+        str = Helper.removeSelectKeyXmlForInsertSql(str);
         
         Pattern xmlTagRegex =  Pattern.compile("<(/?[\\w#@]+)(.*?)>");
         StringBuffer sql = new StringBuffer();
@@ -45,9 +42,9 @@ public class IbatisSqlMapConfigParser {
                 continue;
             }
             
-            processForMybatis(sql, xmlTag, attributes);
+            Helper.processForMybatis(sql, xmlTag, attributes);
             
-            String replacement = getReplacement(attributes.get("open"), attributes.get("prepend"));
+            String replacement = Helper.getReplacement(attributes.get("open"), attributes.get("prepend"));
             StringHelper.appendReplacement(m, sql, replacement);
             
             if(openClose != null && openClose.close != null && xmlTag.equals("/"+openClose.xmlTag)) {
@@ -70,75 +67,70 @@ public class IbatisSqlMapConfigParser {
     	public String xmlTag;
     }
 
-	private static void processForMybatis(StringBuffer sb, String xmlTag,
-			Map<String, String> attributes) {
-		// mybatis <where>
-		if("where".equals(xmlTag.trim())) {
-			sb.append("where");
-		}
-		// mybatis <set>
-		if("set".equals(xmlTag.trim())) {
-			sb.append("set");
-		}
-		// mybatis <foreach collection="usernameList" item="item" index="index" open="(" separator="," close=")"> 
-		if("foreach".equals(xmlTag.trim())) {
-//            	m.appendReplacement(sb, "set"); //FIXME for foreach
-		}
-		// mybatis <trim prefix="" suffix="" prefixOverrides="" suffixOverrides=""></trim>
-		if("trim".equals(xmlTag.trim())) {
-			attributes.put("open", attributes.get("prefix"));
-			attributes.put("close", attributes.get("suffix")); //FIXME for prefixOverrides,suffixOverrides
-		}
-	}
-
-	private static String getReplacement(String open, String prepend) {
-		String replacement = null;
-		if(prepend != null) {
-			replacement = " "+prepend+" "+StringHelper.defaultString(open);
-		}else {
-		    if (StringHelper.isEmpty(open)) {
-		    	replacement = "";
-		    } else {
-		    	replacement = (" " + open);
-		    }
-		}
-		return replacement;
-	}
-    
     //process <include refid="otherSql"/>
-	private void processIncludeByRefid(Map<String, String> includeSqls,
-			StringBuffer sb, Matcher m, Map<String, String> attributes) {
-		String refid = attributes.get("refid");
-		if(refid == null) {
-			 m.appendReplacement(sb, "");
-		}else {
-		    String includeValue = includeSqls.get(refid);
-		    if(includeValue == null) throw new IllegalArgumentException("not found include sql by <include refid='"+refid+"'/>");
-		    StringHelper.appendReplacement(m, sb, parse(includeValue,includeSqls));
-		}
-	}
-	
-	private static String removeSelectKeyXmlForInsertSql(String str) {
-    	if(str == null) return null;
-    	return str.replaceAll("(?s)<selectKey.*?>.*</selectKey>","");
-	}
-
-	public static String removeComments(String str) {
-        if(str == null) return null;
-        str = str.replaceAll("(?s)<!--.*?-->", "").replaceAll("(?s)/\\*.*?\\*/", "").replace("query not allowed", "");
-        return str;
-    }
-
-    public static void main(String[] args) throws IOException {
-    	IbatisSqlMapConfigParser parser = new IbatisSqlMapConfigParser();
-        System.out.println("<abc>123</abc> <diy></diy>".replaceAll("</?\\w*>", ""));
-        System.out.println("parsed:"+parser.parse("<isNotEmpty prepend='and' property='gmtCreateStartTime'>BTR.gmt_create &gt;= #gmtCreateStartTime#</isNotEmpty>"));
-        System.out.println("parsed:"+parser.parse("<dynamic prepend='WHERE 1=1'>123</dynamic>"));
-        System.out.println("parsed:"+parser.parse("select * from user_info"));
-        File file = FileHelper.getFileByClassLoader("cn/org/rapid_framework/generator/ext/tableconfig/test.xml");
-		System.out.println("parsed file:"+parser.parse(IOHelper.readFile(file)));
-		System.out.println("".equals(removeComments("<!--1\n2\n3-->")));
-		System.out.println("".equals(removeComments("/*1\n2\n3*/")));
+    private void processIncludeByRefid(Map<String, String> includeSqls,
+            StringBuffer sb, Matcher m, Map<String, String> attributes) {
+        String refid = attributes.get("refid");
+        if(refid == null) {
+             m.appendReplacement(sb, "");
+        }else {
+            String includeValue = includeSqls.get(refid);
+            if(includeValue == null) throw new IllegalArgumentException("not found include sql by <include refid='"+refid+"'/>");
+            usedIncludedSqls.put(refid, includeValue);
+            StringHelper.appendReplacement(m, sb, parse(includeValue,includeSqls));
+        }
     }
     
+    static class Helper {
+        
+        private static void processForMybatis(StringBuffer sb, String xmlTag,
+                                              Map<String, String> attributes) {
+            // mybatis <where>
+            if ("where".equals(xmlTag.trim())) {
+                sb.append("where");
+            }
+            // mybatis <set>
+            if ("set".equals(xmlTag.trim())) {
+                sb.append("set");
+            }
+            // mybatis <foreach collection="usernameList" item="item"
+            // index="index" open="(" separator="," close=")">
+            if ("foreach".equals(xmlTag.trim())) {
+                // m.appendReplacement(sb, "set"); //FIXME for foreach
+            }
+            // mybatis <trim prefix="" suffix="" prefixOverrides=""
+            // suffixOverrides=""></trim>
+            if ("trim".equals(xmlTag.trim())) {
+                attributes.put("open", attributes.get("prefix"));
+                attributes.put("close", attributes.get("suffix")); // FIXME for
+                                                                   // prefixOverrides,suffixOverrides
+            }
+        }
+        
+    	private static String getReplacement(String open, String prepend) {
+    		String replacement = null;
+    		if(prepend != null) {
+    			replacement = " "+prepend+" "+StringHelper.defaultString(open);
+    		}else {
+    		    if (StringHelper.isEmpty(open)) {
+    		    	replacement = "";
+    		    } else {
+    		    	replacement = (" " + open);
+    		    }
+    		}
+    		return replacement;
+    	}
+    	
+    	private static String removeSelectKeyXmlForInsertSql(String str) {
+        	if(str == null) return null;
+        	return str.replaceAll("(?s)<selectKey.*?>.*</selectKey>","");
+    	}
+    
+    	public static String removeComments(String str) {
+            if(str == null) return null;
+            str = str.replaceAll("(?s)<!--.*?-->", "").replaceAll("(?s)/\\*.*?\\*/", "").replace("query not allowed", "");
+            return str;
+        }
+    }
+
 }
