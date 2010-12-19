f530aaaca0602d0286cb116e1f5b8b658053a597
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-19 19:23:22 +0000

    [maven-release-plugin]  copy for tag 4.0

--- website/themes/StudioPress_Red/adsense_top.php
+++
@@ -1,5 +0,0 @@
-<!-- Insert your adsense code for a 468x60 banner right under here -->
-
-<img src="<?php bloginfo('template_directory'); ?>/images/banner_img.jpg" alt="ad"></img>
-
-
--- website/themes/StudioPress_Red/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div>
-<input type="text" value="<?php the_search_query(); ?>" name="s" id="searchbox" />
-<input type="submit" id="searchbutton" value="" />
-</div>
-</form>
