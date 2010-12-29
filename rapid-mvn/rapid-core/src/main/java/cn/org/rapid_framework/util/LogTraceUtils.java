cf620549291250349d393d3c039f2c12652b5554
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-29 15:53:08 +0000

    add LogTraceUtils

---
+++ rapid-mvn/rapid-core/src/main/java/cn/org/rapid_framework/util/LogTraceUtils.java
@@ -0,0 +1,40 @@
+package cn.org.rapid_framework.util;
+
+import java.util.UUID;
+
+import org.apache.log4j.MDC;
+
+/**
+ * 系统运行时打印方便调试与追踪信息的工具类.
+ * 
+ * 使用MDC存储traceID, 一次trace中所有日志都自动带有该ID,
+ * 可以方便的用grep命令在日志文件中提取该trace的所有日志.
+ * 
+ * 需要在log4j.properties中将ConversionPattern添加%X{traceId},如:
+ * log4j.appender.stdout.layout.ConversionPattern=%d [%c] %X{traceId}-%m%n
+ * 
+ * @author calvin
+ * @authro badqiu
+ */
+public class LogTraceUtils {
+
+	public static final String TRACE_ID_KEY = "traceId";
+
+	/**
+	 * 开始Trace, 默认生成UUID并放入MDC.
+	 * @return traceId
+	 */
+	public static String beginTrace() {
+		String traceId = UUID.randomUUID().toString().replace("-","");
+		MDC.put(TRACE_ID_KEY, traceId);
+		return traceId;
+	}
+
+	/**
+	 * 结束一次Trace.
+	 * 清除traceId.
+	 */
+	public static void endTrace() {
+		MDC.remove(TRACE_ID_KEY);
+	}
+}
