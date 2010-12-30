9421b12a76c4152437c02c5836b1443c99679fd3
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-30 08:04:24 +0000

    springmvc-mybatis initial import

--- rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/mvn-archetype-create-from-project.bat
+++ rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/mvn-archetype-create-from-project.bat
@@ -1,1 +1,7 @@
-mvn archetypecreate-from-project
+call mvn archetype:create-from-project
+
+cd target/generated-sources/archetype
+
+call mvn install
+
+cd ../../../
---
+++ rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/src/test/java/com/company/project/common/PleaseDeleteMe2.java
@@ -0,0 +1,13 @@
+package com.company.project.common;
+
+/**
+ * 用于maven archetype生成项目占坑使用，请删除本文件 
+ * @author badqiu
+ */
+public class PleaseDeleteMe2 {
+    
+    public void say() {
+        System.out.println("please delete me");
+    }
+    
+}
