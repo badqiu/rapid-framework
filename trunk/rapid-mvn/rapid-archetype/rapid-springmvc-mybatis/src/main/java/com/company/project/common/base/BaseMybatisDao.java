72446b6f92214efa4cfcdbe1f541ae7b818ff97b
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-31 16:54:17 +0000

    move

--- trunk/rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/src/main/java/com/company/project/common/base/BaseMybatisDao.java
+++ trunk/rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/src/main/java/com/company/project/common/base/BaseMybatisDao.java
@@ -2,20 +2,17 @@ package com.company.project.common.base;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
-import org.springframework.util.Assert;
-import org.springframework.util.ReflectionUtils;
 
-import cn.org.rapid_framework.beanutils.PropertyUtils;
 import cn.org.rapid_framework.page.Page;
 import cn.org.rapid_framework.page.PageRequest;
 import cn.org.rapid_framework.util.page.PageList;
-import cn.org.rapid_framework.util.page.Paginator;
+
+import com.company.project.common.util.MybatisPageQueryUtils;
 
 /**
  * @author badqiu
@@ -50,24 +47,24 @@ public abstract class BaseMybatisDao<E,PK extends Serializable> extends SqlSessi
     protected void prepareObjectForSaveOrUpdate(E o) {
     }
 
-    public String getIbatisMapperNamesapce() {
+    public String getMybatisMapperNamesapce() {
         throw new RuntimeException("not yet implement");
     }
     
     public String getFindByPrimaryKeyStatement() {
-        return getIbatisMapperNamesapce()+".getById";
+        return getMybatisMapperNamesapce()+".getById";
     }
 
     public String getInsertStatement() {
-        return getIbatisMapperNamesapce()+".insert";
+        return getMybatisMapperNamesapce()+".insert";
     }
 
     public String getUpdateStatement() {
-    	return getIbatisMapperNamesapce()+".update";
+    	return getMybatisMapperNamesapce()+".update";
     }
 
     public String getDeleteStatement() {
-    	return getIbatisMapperNamesapce()+".delete";
+    	return getMybatisMapperNamesapce()+".delete";
     }
     
     public SqlSession getSqlSessionTemplate() {
@@ -78,17 +75,6 @@ public abstract class BaseMybatisDao<E,PK extends Serializable> extends SqlSessi
 		return statementName +".count";
 	}
     
-	protected Page pageQuery(String statementName, PageRequest pageRequest) {
-		PageList pageList = pageQuery(getSqlSession(),statementName,getCountStatementForPaging(statementName),pageRequest);
-        Page page = new Page(pageRequest,pageList.getTotalItems());
-        page.setResult(pageList);
-        return page;
-	}
-	
-	public static PageList pageQuery(SqlSession sqlSessionTemplate,String statementName,String countStatementName, PageRequest pageRequest) {
-		return MybatisPageQueryUtils.pageQuery(sqlSessionTemplate, statementName, countStatementName,pageRequest, pageRequest.getPageNumber(), pageRequest.getPageSize());
-	}
-	
 	public List findAll() {
 		throw new UnsupportedOperationException();
 	}
@@ -101,53 +87,4 @@ public abstract class BaseMybatisDao<E,PK extends Serializable> extends SqlSessi
 		//ignore
 	}
 	
-	public static class MybatisPageQueryUtils {
-		
-		public static PageList pageQuery(SqlSession sqlSession, String statement,
-				Object parameter, int pageNo, int pageSize) {
-			String countStatement = statement + ".count";
-
-			return pageQuery(sqlSession, statement, countStatement, parameter, pageNo,
-					pageSize);
-		}
-
-		public static PageList pageQuery(SqlSession sqlSession, String statement,
-				String countStatement, Object parameter, int pageNo,
-				int pageSize) {
-			Assert.isTrue(pageSize > 0,"pageSize > 0 must be true");
-			
-			Number totalItems = (Number) sqlSession.selectOne(countStatement,parameter);
-
-			if (totalItems != null && totalItems.intValue() > 0) {
-				Paginator paginator = new Paginator(pageNo, pageSize, totalItems.intValue());
-				List list = sqlSession.selectList(statement, attachPageQueryVariable(parameter,paginator));
-				PageList page = new PageList(list,paginator);
-				return page;
-			}
-
-			return new PageList(0,pageSize,0);
-		}
-
-		public static Map attachPageQueryVariable(Object parameter,Paginator p) {
-			Map map = toParameterMap(parameter);
-			map.put("startRow", p.getStartRow());
-			map.put("endRow", p.getEndRow());
-			map.put("offset", p.getOffset());
-			map.put("limit", p.getLimit());
-			return map;
-		}
-		
-		public static Map toParameterMap(Object parameter) {
-			if(parameter instanceof Map) {
-				return (Map)parameter;
-			}else {
-				try {
-					return PropertyUtils.describe(parameter);
-				} catch (Exception e) {
-					ReflectionUtils.handleReflectionException(e);
-					return null;
-				}
-			}
-		}
-	}
 }
