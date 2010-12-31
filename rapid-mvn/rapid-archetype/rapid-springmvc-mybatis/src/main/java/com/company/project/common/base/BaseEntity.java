506c8e0e649951a0615e46471238ec23fcf0b24d
badqiu <badqiu@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2010-12-31 16:57:47 +0000

    extract DateFormats

--- rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/src/main/java/com/company/project/common/base/BaseEntity.java
+++ rapid-mvn/rapid-archetype/rapid-springmvc-mybatis/src/main/java/com/company/project/common/base/BaseEntity.java
@@ -7,13 +7,5 @@ package com.company.project.common.base;
 public class BaseEntity implements java.io.Serializable {
 
 	private static final long serialVersionUID = -7200095849148417467L;
-
-	protected static final String DATE_FORMAT = "yyyy-MM-dd";
-	
-	protected static final String TIME_FORMAT = "HH:mm:ss";
-	
-	protected static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-	
-	protected static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.S";
 	
 }
