4672cef0a5e6350311545d5ca405d9b965bcef1a
hhlai1990 <hhlai1990@4ca5cf0f-1a52-0410-96e1-5992aa8baeaf>
2011-07-01 02:43:48 +0000

    修改rapid-plugin ，重命名，以及增加运行时project依赖获取功能，即：若当前工程配置了jdbc driver ，则plugin classloader可以在运行时加载

--- trunk/rapid-mvn/rapid-plugins/src/main/java/cn/org/rapid_framework/plugins/gen/GeneratorByTableMojo.java
+++ trunk/rapid-mvn/rapid-plugins/src/main/java/cn/org/rapid_framework/plugins/gen/GeneratorByTableMojo.java
@@ -3,9 +3,16 @@
  */
 package cn.org.rapid_framework.plugins.gen;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 
 import cn.org.rapid_framework.generator.GeneratorFacade;
 import cn.org.rapid_framework.generator.GeneratorProperties;
@@ -14,14 +21,15 @@ import cn.org.rapid_framework.generator.GeneratorProperties;
  * 代码生成器插件的主要goal
  * 
  * @author hunhun
- * @goal del
+ * @goal gen
  */
-public class DelMojo extends BaseMojo {
+public class GeneratorByTableMojo extends AbstarctGeneratorMojo {
 
 	/**
 	 * -Dtable=* -Dtable=user_info
 	 * 
 	 * @parameter expression="${table}"
+	 * 
 	 */
 	public String table;
 
@@ -36,35 +44,32 @@ public class DelMojo extends BaseMojo {
 
 		Thread currentThread = Thread.currentThread();
 		ClassLoader oldClassLoader = currentThread.getContextClassLoader();
+
 		try {
 			currentThread.setContextClassLoader(getClassLoader());
-
-			System.out.println("current project to build: " + getProject().getName());
+			System.out.println("current project to build: " + getProject().getName() + "\n"
+					+ getProject().getFile().getParent());
 			g = new GeneratorFacade();
 			g.getGenerator().setTemplateRootDirs("classpath:template");
-			delByTable(table);
+			try {
+				g.deleteOutRootDir();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			genByTable(table);
 		} finally {
 			currentThread.setContextClassLoader(oldClassLoader);
 		}
+
 	}
 
-	public void delByTable(String... table) {
+	public void genByTable(String... table) {
 		try {
-			g.deleteByTable(table);
+			g.generateByTable(table);
 			Runtime.getRuntime().exec("cmd.exe /c start " + GeneratorProperties.getRequiredProperty("outRoot"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	// public static void main(String[] args) {
-	// DelMojo t = new DelMojo();
-	// t.g = new GeneratorFacade();
-	// t.g.getGenerator().setTemplateRootDirs("classpath:template",
-	// "classpath:generator/template/rapid/share");
-	//
-	// t.delByTable("user_info", "user_role");
-	// t.delByTable("*");
-	// }
 
 }
